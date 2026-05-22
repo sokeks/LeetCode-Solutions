dc5fc3726090f411d98f911f9f6eba2b323786cd
sokeks <131011732+sokeks@users.noreply.github.com>
2026-05-22 14:55:48 +0200

    Time: 1 ms (99.95%), Space: 37 MB (70.8%) - LeetHub

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+        {
+            return false;
+        }
+
+        var rev = 0;
+        while (x > rev)
+        {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == rev || x == rev / 10;
+    }
+}
