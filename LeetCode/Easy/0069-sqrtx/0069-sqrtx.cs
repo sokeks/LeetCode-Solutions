cab9765d522aa149f65e918d7e5b0648abca8830
sokeks <131011732+sokeks@users.noreply.github.com>
2026-05-22 15:13:54 +0200

    Time: 0 ms (100%), Space: 29.2 MB (33.48%) - LeetHub

---
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int MySqrt(int x) {
+        long left = 0;
+        long right = 50000l; // bo max(x) = 2^31 -> 2 * 10^9 -> 20 * 10^8 sqrt(x) = sqrt (20 * 10^8 ) ~= 4,5... * 10^4 ~ 5 * 10^4
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            var mul = mid * mid;
+
+            if (mul == x)
+            {
+                return (int)mid;
+            }
+            else if (mul > x)
+            {
+                right = mid - 1;
+            }
+            else // mul < x
+            {
+                left = mid + 1;
+            }
+        }
+        
+        return (int)left - 1;
+    }
+}
