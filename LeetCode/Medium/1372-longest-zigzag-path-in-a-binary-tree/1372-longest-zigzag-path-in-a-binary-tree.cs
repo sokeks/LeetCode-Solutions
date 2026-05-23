a6ac1583e52cc8977b9cd6afb9e6628c56844b79
sokeks <131011732+sokeks@users.noreply.github.com>
2026-05-23 11:30:02 +0200

    Time: 220 ms (5.77%), Space: 54.2 MB (99.04%) - LeetHub

--- LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cs
+++ LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cs
@@ -12,28 +12,59 @@
  * }
  */
 public class Solution {
-    enum Directions
-    {
-        none,
-        left,
-        right
-    }
-
     public int LongestZigZag(TreeNode root) {
-        var stack = new Stack<(TreeNode node, int zigZagCount, Directions dir)>();
-        stack.Push((root, 0, Directions.none));
+        const int reasonableStartStackSize = 32;
+        var stack = new Stack<(TreeNode node, int zigZagCountIfComesFromLeft, int zigZagCountIfComesFromRight)>(reasonableStartStackSize);
+        stack.Push((root, 0, 0));
 
         var maxZigZagCount = 0;
         while (stack.Count > 0)
         {
-            var (node, zigZagCount, dir) = stack.Pop();
+            var (node, zigZagCountIfComesFromLeft, zigZagCountIfComesFromRight) = stack.Pop();
 
-            maxZigZagCount = Math.Max(zigZagCount, maxZigZagCount);
-
-            if (node.right != null) stack.Push((node.right, dir == Directions.left ? zigZagCount + 1 : 1, Directions.right));
-            if (node.left != null) stack.Push((node.left, dir == Directions.right ? zigZagCount + 1 : 1, Directions.left));
+            if (node.right != null)
+            {
+                var zigZacCountToRight = zigZagCountIfComesFromLeft + 1;
+                maxZigZagCount = Math.Max(zigZacCountToRight, maxZigZagCount);
+                stack.Push((node.right, 0, zigZacCountToRight));
+            }
+            if (node.left != null)
+            {
+                var zigZacCountToLeft = zigZagCountIfComesFromRight + 1;
+                maxZigZagCount = Math.Max(zigZacCountToLeft, maxZigZagCount);
+                stack.Push((node.left, zigZacCountToLeft, 0));
+            }
         }
 
         return maxZigZagCount;
     }
+
+
+
+    // *** my first solution ***
+    // enum Directions
+    // {
+    //     none,
+    //     left,
+    //     right
+    // }
+
+    // public int LongestZigZag(TreeNode root) {
+    //     const int reasonableStartStackSize = 32;
+    //     var stack = new Stack<(TreeNode node, int zigZagCount, Directions dir)>(reasonableStartStackSize);
+    //     stack.Push((root, 0, Directions.none));
+
+    //     var maxZigZagCount = 0;
+    //     while (stack.Count > 0)
+    //     {
+    //         var (node, zigZagCount, dir) = stack.Pop();
+
+    //         maxZigZagCount = Math.Max(zigZagCount, maxZigZagCount);
+
+    //         if (node.right != null) stack.Push((node.right, dir == Directions.left ? zigZagCount + 1 : 1, Directions.right));
+    //         if (node.left != null) stack.Push((node.left, dir == Directions.right ? zigZagCount + 1 : 1, Directions.left));
+    //     }
+
+    //     return maxZigZagCount;
+    // }
 }
