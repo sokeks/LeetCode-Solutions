6f40493909ad61fdd0f29c9ccd9c22f36d97de4b
sokeks <131011732+sokeks@users.noreply.github.com>
2026-05-26 07:53:21 +0200

    Time: 1 ms (5.75%), Space: 51.4 MB (57.51%) - LeetHub

---
+++ LeetCode/Easy/0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        var stack = new Stack<TreeNode>();
+        stack.Push(root);
+
+        while (stack.Count > 0)
+        {
+            var current = stack.Pop();
+            if (current.val == val)
+            {
+                return current;
+            }
+
+            if (current.right != null) stack.Push(current.right);
+            if (current.left != null) stack.Push(current.left);
+        }
+
+        return null;
+    }
+}
