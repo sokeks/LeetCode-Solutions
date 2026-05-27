cbbbce2adb07c2b8f5c61b10b74aa634afbc7974
sokeks <131011732+sokeks@users.noreply.github.com>
2026-05-27 08:48:37 +0200

    Time: 1 ms (99.49%), Space: 47.4 MB (83.08%) - LeetHub

--- LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.cs
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.cs
@@ -1,21 +1,24 @@
 public class Solution {
     public bool CanVisitAllRooms(IList<IList<int>> rooms) {
         var keysToTry = new Stack<int>(rooms.Count);
-        keysToTry.Push(0);
         Span<bool> visitedRooms = rooms.Count <= 1024 ? stackalloc bool[rooms.Count] : new bool[rooms.Count];
-        var visitedRoomsCount = 0;
+
+        keysToTry.Push(0);
+        visitedRooms[0] = true;
+        var visitedRoomsCount = 1;
 
         while (keysToTry.Count > 0 && visitedRoomsCount != rooms.Count)
         {
             var key = keysToTry.Pop();
-            if (!visitedRooms[key])
-            {
-                visitedRooms[key] = true;
-                visitedRoomsCount++;
-            }
             for (var i = 0; i < rooms[key].Count; i++)
             {
-                if (!visitedRooms[rooms[key][i]]) keysToTry.Push(rooms[key][i]);
+                var nextRoom = rooms[key][i];
+                if (!visitedRooms[nextRoom])
+                {
+                    visitedRooms[nextRoom] = true;
+                    visitedRoomsCount++;
+                    keysToTry.Push(nextRoom);
+                }
             }
         }
 
