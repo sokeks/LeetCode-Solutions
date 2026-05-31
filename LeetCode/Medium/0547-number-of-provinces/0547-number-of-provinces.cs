5cba39e6148f8a89d000749ab63146582be1c211
sokeks <131011732+sokeks@users.noreply.github.com>
2026-05-31 20:54:55 +0200

    Time: 5 ms (44.59%), Space: 45.5 MB (78.36%) - LeetHub

--- LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.cs
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.cs
@@ -1,32 +1,73 @@
 public class Solution {
     public int FindCircleNum(int[][] isConnected) {
-        var provincesCount = 0;
+        Span<int> parent = isConnected.Length <= 1024 ? stackalloc int[isConnected.Length] : new int[isConnected.Length];
 
-        var townsToVisit = new Stack<int>(isConnected.Length);
-        Span<bool> visitedTowns = isConnected.Length <= 1024 ? stackalloc bool[isConnected.Length] : new bool[isConnected.Length];
-
-        for (var i = 0; i < isConnected.Length ; i++)
+        for (var i = 0; i < isConnected.Length; i++)
         {
-            if (visitedTowns[i]) continue;
+            parent[i] = i;
+        }
 
-            visitedTowns[i] = true;
-            townsToVisit.Push(i);
-            provincesCount++;
-
-            while (townsToVisit.Count > 0)
+        for (var i = 0; i < isConnected.Length; i++)
+        {
+            for (var j = i + 1; j < isConnected.Length; j++)
             {
-                var visitingTown = townsToVisit.Pop();
-                for (var j = 0; j < isConnected[visitingTown].Length; j++)
-                {
-                    if (isConnected[visitingTown][j] == 1 && !visitedTowns[j])
-                    {
-                        visitedTowns[j] = true;
-                        townsToVisit.Push(j);
-                    }
-                }
+                if (isConnected[i][j] == 1) Union(i, j, parent);
             }
         }
-        
+
+        var provincesCount = 0;
+
+        for (var i = 0; i < parent.Length; i++)
+        {
+            if (parent[i] == i) provincesCount++;
+        }
+
         return provincesCount;
+        static void Union(int i, int j, Span<int> parent)
+        {
+            var iParent = Find(i, parent);
+            var jParent = Find(j, parent);
+
+            parent[iParent] = jParent;
+        }
+
+        static int Find(int i, Span<int> parent)
+        {
+            if (parent[i] == i) return i;
+
+            return Find(parent[i], parent);
+        }
     }
+
+    // *** my first solution ***
+    // public int FindCircleNum(int[][] isConnected) {
+    //     var provincesCount = 0;
+
+    //     var townsToVisit = new Stack<int>(isConnected.Length);
+    //     Span<bool> visitedTowns = isConnected.Length <= 1024 ? stackalloc bool[isConnected.Length] : new bool[isConnected.Length];
+
+    //     for (var i = 0; i < isConnected.Length ; i++)
+    //     {
+    //         if (visitedTowns[i]) continue;
+
+    //         visitedTowns[i] = true;
+    //         townsToVisit.Push(i);
+    //         provincesCount++;
+
+    //         while (townsToVisit.Count > 0)
+    //         {
+    //             var visitingTown = townsToVisit.Pop();
+    //             for (var j = 0; j < isConnected[visitingTown].Length; j++)
+    //             {
+    //                 if (isConnected[visitingTown][j] == 1 && !visitedTowns[j])
+    //                 {
+    //                     visitedTowns[j] = true;
+    //                     townsToVisit.Push(j);
+    //                 }
+    //             }
+    //         }
+    //     }
+        
+    //     return provincesCount;
+    // }
 }
