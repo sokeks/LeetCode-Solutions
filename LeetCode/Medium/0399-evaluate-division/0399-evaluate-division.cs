67cb13a59a46a098f1001928ee0c317128cc3798
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-02 10:48:25 +0200

    Time: 3 ms (77.57%), Space: 47.5 MB (87.85%) - LeetHub

--- LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.cs
+++ LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.cs
@@ -4,39 +4,13 @@ public class Solution {
         for (var i = 0; i < equations.Count; i++) AddEquationToAdjacency(equations[i], values[i], adjacency);
 
         var answers = new double[queries.Count];
-        foreach (ref double answer in answers.AsSpan()) answer = -1;
         for (var i = 0; i < queries.Count; i++)
         {
             var query = queries[i];
-            if (!adjacency.ContainsKey(query[0]) || !adjacency.ContainsKey(query[1])) continue;
-            if (query[0] == query[1])
-            {
-                answers[i] = 1;
-                continue;
-            }
 
-            var nextVariables = new Stack<(string variable, double result)>();
-            nextVariables.Push((query[0], 1.0));
-            var visitedVariables = new HashSet<string>();
-            visitedVariables.Add(query[0]);
-            while (answers[i] == -1 && nextVariables.Count > 0)
-            {
-                var (variable, currentResult) = nextVariables.Pop();
-
-                foreach (var (neighbour, weight) in adjacency[variable])
-                {
-                    if (visitedVariables.Contains(neighbour)) continue;
-
-                    var result = currentResult * weight;
-                    if (neighbour == query[1])
-                    {
-                        answers[i] = result;
-                        break;
-                    }
-                    nextVariables.Push((neighbour, result));
-                    visitedVariables.Add(neighbour);
-                }
-            }
+            answers[i] = !adjacency.ContainsKey(query[0]) || !adjacency.ContainsKey(query[1])
+                ? -1
+                : CalculateAnswerToQuery(adjacency, query);
         }
 
         return answers;
@@ -57,5 +31,34 @@ public class Solution {
                 adjacency[start] = new List<(string, double)> { (end, weight) };
             }
         }
+    }
+
+    private double CalculateAnswerToQuery(Dictionary<string, List<(string neighbour, double weight)>> adjacency, IList<string> query)
+    {
+        if (query[0] == query[1]) return 1;
+
+        var nextVariables = new Stack<(string variable, double result)>();
+        nextVariables.Push((query[0], 1.0));
+        var visitedVariables = new HashSet<string>();
+        visitedVariables.Add(query[0]);
+        while (nextVariables.Count > 0)
+        {
+            var (variable, currentResult) = nextVariables.Pop();
+
+            foreach (var (neighbour, weight) in adjacency[variable])
+            {
+                if (visitedVariables.Contains(neighbour)) continue;
+
+                var result = currentResult * weight;
+                if (neighbour == query[1])
+                {
+                    return result;
+                }
+                nextVariables.Push((neighbour, result));
+                visitedVariables.Add(neighbour);
+            }
+        }
+
+        return -1;
     }
 }
