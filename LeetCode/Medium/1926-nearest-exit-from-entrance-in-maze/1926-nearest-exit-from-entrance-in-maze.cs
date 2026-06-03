7e48bdb0355c94c11fb0d9e89bc2d49239d43409
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-03 20:53:40 +0200

    Time: 12 ms (33.33%), Space: 58.9 MB (74.51%) - LeetHub

--- LeetCode/Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cs
+++ LeetCode/Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cs
@@ -1,39 +1,34 @@
 public class Solution {
     public int NearestExit(char[][] maze, int[] entrance) {
         var nextCells = new Queue<(int x, int y, int stepsMade)>();
-        var cellsVisited = new bool[maze.Length][];
-        for (var i = 0; i < maze.Length; i++) cellsVisited[i] = new bool[maze[0].Length];
+        var cellsVisited = new bool[maze.Length * maze[0].Length];
 
-        AddStep(entrance[0], entrance[1], -1, nextCells, cellsVisited);
+        nextCells.Enqueue((entrance[0], entrance[1], 0));
+        cellsVisited[getVisitedIndex(entrance[0], entrance[1], maze[0].Length)] = true;
 
         while (nextCells.Count > 0)
         {
-            var (x, y, stepsMade) = nextCells.Dequeue();
-            // Console.WriteLine($"{x}, {y}");
-            if (x != entrance[0] || y != entrance[1])
+            var (x0, y0, stepsMade) = nextCells.Dequeue();
+
+            var directions = new (int, int)[] { (x0 - 1, y0), (x0 + 1, y0), (x0, y0 - 1), (x0, y0 + 1) };
+            foreach (var (x, y) in directions)
             {
-                if (x == 0 || x == maze.Length - 1 || y == 0 || y == maze[0].Length - 1) return stepsMade;
+                // for (var i = 0; i < cellsVisited.Length; i++) Console.WriteLine(i + ":" + cellsVisited[i]);
+                if (isAllowed(x, y, maze, cellsVisited))
+                {
+                    if (isOnEdge(x, y, maze)) return stepsMade + 1;
+                    nextCells.Enqueue((x, y, stepsMade + 1));
+                    cellsVisited[getVisitedIndex(x, y, maze[0].Length)] = true;
+                }                
             }
-
-
-            var up = x - 1;
-            if (up >= 0 && maze[up][y] == '.' && !cellsVisited[up][y]) AddStep(up, y, stepsMade, nextCells, cellsVisited);
-            
-            var down = x + 1;
-            if (down < maze.Length && maze[down][y] == '.' && !cellsVisited[down][y]) AddStep(down, y, stepsMade, nextCells, cellsVisited);
-            
-            var left = y - 1;
-            if (left >= 0 && maze[x][left] == '.' && !cellsVisited[x][left]) AddStep(x, left, stepsMade, nextCells, cellsVisited);
-            
-            var right = y + 1;
-            if (right < maze[0].Length && maze[x][right] == '.' && !cellsVisited[x][right]) AddStep(x, right, stepsMade, nextCells, cellsVisited);
         }
 
         return -1;
-        static void AddStep(int x, int y, int stepsMade, Queue<(int x, int y, int stepsMade)> nextCells, bool[][] cellsVisited)
-        {
-            nextCells.Enqueue((x, y, stepsMade + 1));
-            cellsVisited[x][y] = true;
-        }
+        static bool isAllowed(int x, int y, char[][] maze, bool[] cellsVisited)
+            => x >= 0 && x < maze.Length && y >= 0 && y < maze[0].Length && maze[x][y] == '.' && !cellsVisited[getVisitedIndex(x, y, maze[0].Length)];
+        static bool isOnEdge(int x, int y, char[][] maze)
+            => x == 0 || x == maze.Length - 1 || y == 0 || y == maze[0].Length - 1;
+        static int getVisitedIndex(int x, int y, int length)
+            => x * length + y;
     }
 }
