523e399f796eebd3ade109100a1854db00c29527
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-04 14:08:10 +0200

    Time: 6 ms (88.24%), Space: 58.9 MB (62.74%) - LeetHub

--- LeetCode/Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cs
+++ LeetCode/Medium/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public int NearestExit(char[][] maze, int[] entrance) {
         var nextCells = new Queue<(int x, int y, int stepsMade)>();
-        var cellsVisited = new bool[maze.Length * maze[0].Length];
+        var cellsVisited = new CellsVisited(maze);
 
         nextCells.Enqueue((entrance[0], entrance[1], 0));
-        cellsVisited[getVisitedIndex(entrance[0], entrance[1], maze)] = true;
+        cellsVisited[entrance[0], entrance[1]] = true;
         ReadOnlySpan<(int, int)> offsets = [(- 1, 0), (1, 0), (0, -1), (0, 1)];
 
         while (nextCells.Count > 0)
@@ -15,21 +15,37 @@ public class Solution {
             {
                 var newX = x + horizontal;
                 var newY = y + vertical;
-                if (isAllowed(newX, newY, maze, cellsVisited))
+                if (IsAllowed(newX, newY, maze, cellsVisited))
                 {
-                    if (isOnEdge(newX, newY, maze)) return stepsMade + 1;
+                    if (IsOnEdge(newX, newY, maze)) return stepsMade + 1;
                     nextCells.Enqueue((newX, newY, stepsMade + 1));
-                    cellsVisited[getVisitedIndex(newX, newY, maze)] = true;
+                    cellsVisited[newX, newY] = true;
                 }                
             }
         }
 
         return -1;
-        static bool isAllowed(int x, int y, char[][] maze, bool[] cellsVisited)
-            => x >= 0 && x < maze.Length && y >= 0 && y < maze[0].Length && maze[x][y] == '.' && !cellsVisited[getVisitedIndex(x, y, maze)];
-        static bool isOnEdge(int x, int y, char[][] maze)
+        static bool IsAllowed(int x, int y, char[][] maze, CellsVisited cellsVisited)
+            => x >= 0 && x < maze.Length && y >= 0 && y < maze[0].Length && maze[x][y] == '.' && !cellsVisited[x, y];
+        static bool IsOnEdge(int x, int y, char[][] maze)
             => x == 0 || x == maze.Length - 1 || y == 0 || y == maze[0].Length - 1;
-        static int getVisitedIndex(int x, int y, char[][] maze)
-            => x * maze[0].Length + y;
+    }
+
+    public class CellsVisited
+    {
+        private readonly int _columnsCount;
+        private readonly bool[] _memory;
+
+        public CellsVisited(char[][] maze)
+        {
+            _columnsCount = maze[0].Length;
+            _memory = new bool[maze.Length * _columnsCount];
+        }
+
+        public bool this[int row, int col]
+        {
+            get => _memory[row * _columnsCount + col];
+            set => _memory[row * _columnsCount + col] = value;
+        }
     }
 }
