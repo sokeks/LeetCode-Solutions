d7501742a20e148707fdb0cd9441b11dbb19fb3e
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-07 12:02:16 +0200

    Time: 1259 ms (8.43%), Space: 55.7 MB (79.7%) - LeetHub

--- LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
@@ -5,9 +5,9 @@ public class Solution {
 
         var kIdx = copy.Length - k;
 
-        return ThreeWayQuickSelect(copy, kIdx);
+        return QuickSelect(copy, kIdx);
 
-        static int ThreeWayQuickSelect(Span<int> nums, int kIdx)
+        static int QuickSelect(Span<int> nums, int kIdx)
         {
             var left = 0;
             var right = nums.Length;
@@ -18,6 +18,7 @@ public class Solution {
 
                 if (kIdx >= pivotStartIdx && kIdx <= pivotEndIdx)
                 {
+                    // Console.WriteLine($"{kIdx} / {pivotStartIdx} / {pivotEndIdx}");
                     return nums[kIdx];
                 }
                 else if (kIdx > pivotEndIdx)
@@ -31,36 +32,61 @@ public class Solution {
             }
 
             throw new System.Diagnostics.UnreachableException("Flaw in the Algorithm");
+            // --- adjusted Partition function from 3 ways QuickSelect for simple QuickSelect ---
             static (int pivotStartIdx, int pivotEndIdx) Partition(Span<int> nums, int left, int right)
             {
-                var pivotIdx = Random.Shared.Next(left, right);
-                var pivot = nums[pivotIdx];
+                // Console.WriteLine($"{left} | {right} | {string.Join(',', nums.ToArray())}");
+                right = right - 1;  // moving from exclusive range to inclusive
+                var pivot = nums[right];
+                var storeIdx = left;
 
-                var lessThanIdx = left;
-                var i = left;
-                var greaterThanIdx = right - 1;
-
-                while (i <= greaterThanIdx)
+                for (var i = left; i < right; i++)
                 {
+                    // Console.WriteLine($"{i}, {storeIdx}");
+
                     if (nums[i] < pivot)
                     {
-                        (nums[i], nums[lessThanIdx]) = (nums[lessThanIdx], nums[i]);
-                        lessThanIdx++;
-                        i++;
-                    }
-                    else if (nums[i] > pivot)
-                    {
-                        (nums[i], nums[greaterThanIdx]) = (nums[greaterThanIdx], nums[i]);
-                        greaterThanIdx--;
-                    }
-                    else // nums[i] == pivot
-                    {
-                        i++;
+                        (nums[i], nums[storeIdx]) = (nums[storeIdx], nums[i]);
+                        storeIdx++;
                     }
                 }
+                (nums[storeIdx], nums[right]) = (nums[right], nums[storeIdx]);
+                // Console.WriteLine($"{left} | {right} | {string.Join(',', nums.ToArray())}");
 
-                return (lessThanIdx, greaterThanIdx);
+                return (storeIdx, storeIdx);
+
             }
+            // --- original Partition function for 3 ways QuickSelect (Dutch Flag algoirthm) ---
+            // static (int pivotStartIdx, int pivotEndIdx) Partition(Span<int> nums, int left, int right)
+            // {
+            //     var pivotIdx = Random.Shared.Next(left, right);
+            //     var pivot = nums[pivotIdx];
+
+            //     var lessThanIdx = left;
+            //     var i = left;
+            //     var greaterThanIdx = right - 1;
+
+            //     while (i <= greaterThanIdx)
+            //     {
+            //         if (nums[i] < pivot)
+            //         {
+            //             (nums[i], nums[lessThanIdx]) = (nums[lessThanIdx], nums[i]);
+            //             lessThanIdx++;
+            //             i++;
+            //         }
+            //         else if (nums[i] > pivot)
+            //         {
+            //             (nums[i], nums[greaterThanIdx]) = (nums[greaterThanIdx], nums[i]);
+            //             greaterThanIdx--;
+            //         }
+            //         else // nums[i] == pivot
+            //         {
+            //             i++;
+            //         }
+            //     }
+
+            //     return (lessThanIdx, greaterThanIdx);
+            // }
         }
     }
 }
