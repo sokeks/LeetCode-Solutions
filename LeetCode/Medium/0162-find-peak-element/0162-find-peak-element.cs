bc9038e1fb048b812a5214f18ecd84ca84cd6deb
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-12 10:14:37 +0200

    Time: 0 ms (100%), Space: 42.5 MB (76.07%) - LeetHub

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.cs
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.cs
@@ -26,15 +26,10 @@ public class Solution {
                     right = mid - 1;
                 }
             }
-            else if (mid == 0)
+            else // if (mid == 0)
             {
                 if (nums[mid] > nums[mid + 1]) return mid;
                 else left = mid + 1;
-            }
-            else // if mid == nums.Length - 1
-            {
-                if (nums[mid] > nums[mid - 1]) return mid;
-                else right = mid - 1;
             }
         }
 
