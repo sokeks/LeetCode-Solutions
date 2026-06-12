002795ac4103bcd3d9883e5766f0d47a684d4c66
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-12 10:06:58 +0200

    Time: 0 ms (100%), Space: 43 MB (23.39%) - LeetHub

---
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int FindPeakElement(int[] nums) {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+
+            if (mid != 0 && mid != nums.Length - 1)
+            {
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                {
+                    return mid;
+                }
+                else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            else if (mid == 0)
+            {
+                if (nums.Length == 1 || nums[mid] > nums[mid + 1]) return mid;
+                else left = mid + 1;
+            }
+            else // if mid == nums.Length - 1
+            {
+                if (nums[mid] > nums[mid - 1]) return mid;
+                else right = mid - 1;
+            }
+        }
+
+        throw new System.Diagnostics.UnreachableException();
+    }
+}
