9cb758b81fad80ddea0b4f37790af8d2d7a0ec49
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-12 10:22:26 +0200

    Time: 0 ms (100%), Space: 42.9 MB (37.86%) - LeetHub

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.cs
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.cs
@@ -1,38 +1,25 @@
 public class Solution {
     public int FindPeakElement(int[] nums) {
-        if (nums.Length == 1) return 0;
-
         var left = 0;
         var right = nums.Length - 1;
 
-        while (left < right)
+        while (left < right) // makes ending up at (nums.length - 1) impossible to achieve
         {
             var mid = left + (right - left) / 2;
-
-            // Console.WriteLine($"{left}|{right}->{mid}");
-
-            if (mid != 0 && mid != nums.Length - 1)
+            if (nums[mid] > nums[mid + 1])
             {
-                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
-                {
-                    return mid;
-                }
-                else if (nums[mid] > nums[mid - 1])
-                {
-                    left = mid + 1;
-                }
-                else
-                {
-                    right = mid - 1;
-                }
+                if (mid == 0 || nums[mid] > nums[mid - 1]) return mid;
+                right = mid - 1;
             }
-            else // if (mid == 0)
+            else
             {
-                if (nums[mid] > nums[mid + 1]) return mid;
-                else left = mid + 1;
+                left = mid + 1;
             }
         }
 
         return left;
     }
 }
+
+
+            // Console.WriteLine($"{left}|{right}->{mid}");
