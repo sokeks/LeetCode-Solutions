5046c3740ddf63a0b4097da62ccbb2d421a72033
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-17 10:48:42 +0200

    Time: 1 ms (38.3%), Space: 48.1 MB (13.33%) - LeetHub

--- LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cs
+++ LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cs
@@ -1,9 +1,40 @@
 public class Solution {
     public IList<string> LetterCombinations(string digits) {
+        string[] keyMap = new string[] { "", "",  "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        var combinations = new string[CalculateTotalCombinations(digits, keyMap)];
+        
+        var last = digits.Length - 1;
+        var stack = new Stack<(char letter, int position)>();
+        foreach (var c in keyMap[digits[0] - '0']) stack.Push((c, 0));
+
+        var sb = new StringBuilder(digits.Length);
+        var combinationsCount = 0;
+        while (stack.Count > 0)
+        {
+            var (letter, position) = stack.Pop();
+            sb.Length = position;
+            sb.Append(letter);
+            if (position == last) combinations[combinationsCount++] = sb.ToString();
+            else foreach (var c in keyMap[digits[position + 1] - '0']) stack.Push((c, position + 1));
+        }
+
+        return combinations;
+        static int CalculateTotalCombinations(string digits, string[] keyMap)
+        {
+            var length = 1;
+            foreach (var d in digits) length *= keyMap[d - '0'].Length;
+
+            return length;
+        } 
+    }
+
+    // --- recursive option, simpler to write (and to invent), but is not a bullet-proof for a very long input --
+    public IList<string> LetterCombinationsRec(string digits) {
         var results = new List<string>();
         LetterCombinations(digits, 0, new StringBuilder(), results);
         return results;
     }
+
 
     public void LetterCombinations(string digits, int idx, StringBuilder sb, IList<string> results) {
         if (idx >= digits.Length)
