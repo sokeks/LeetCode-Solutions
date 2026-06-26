1a91a1c2b22dfa083b8c293effd5d66a9af36fde
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-26 08:57:41 +0200

    Time: 0 ms (100%), Space: 19.4 MB (7.96%) - LeetHub

--- LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.py
+++ LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,29 +1,35 @@
 
-class Item(NamedTuple):
+class State(NamedTuple):
     number: int
     position: int
     current_sum: int
 
 class Solution:
-    def _populate_stack_with_next_numbers(self, stack : Deque[Item], current: Item, target_k: int, target_sum: int):
+    def _generate_next_states(self, current: State, target_k: int, target_sum: int):
         for i in range(current.number + 1, 10 - ((target_k - 1) - (current.position + 1))):
             new_sum = current.current_sum + i
             if new_sum > target_sum: break
-            stack.append(Item(number=i, position=current.position + 1, current_sum=new_sum))
+            yield State(number=i, position=current.position + 1, current_sum=new_sum)
+
+    def _add_next_states(self, stack: Deque[State], current_state: State, k: int, n: int):
+        for next_state in self._generate_next_states(current_state, k, n):
+            stack.append(next_state)
 
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         stack = deque()
-        self._populate_stack_with_next_numbers(stack, Item(number=0, position=-1, current_sum=0), k, n)
+        self._add_next_states(stack, State(number=0, position=-1, current_sum=0), k, n)
+        # for next_state in self._generate_next_states(State(number=0, position=-1, current_sum=0), k, n):
+        #     stack.append(next_state)
 
         combinations = []
         elements = [None] * k
         while stack:
-            item = stack.pop()
-            elements[item.position] = item.number
-            if item.position == k - 1:
-                if item.current_sum == n:
+            state = stack.pop()
+            elements[state.position] = state.number
+            if state.position == k - 1:
+                if state.current_sum == n:
                     combinations.append(elements.copy())
                 continue
-            self._populate_stack_with_next_numbers(stack, item, k, n)
+            self._add_next_states(stack, state, k, n)
 
         return combinations
