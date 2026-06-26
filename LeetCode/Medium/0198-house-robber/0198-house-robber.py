4d6627968b53799014d33c692ebee5d9035ea9e8
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-26 20:03:11 +0200

    Time: 0 ms (100%), Space: 19.2 MB (89.71%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.py
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -2,13 +2,14 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
         if len(nums) == 2: return max(nums[0], nums[1])
-        if len(nums) == 3: return max(nums[0] + nums[2], nums[1])
-        prev3 = 0
         prev2 = 0
-        prev1 = nums[0]
+        prev1 = 0
 
-        for i in range(3, len(nums)):
-            prev3, prev2, prev1 = prev2, prev1, max(prev2 + nums[i - 2], prev3 + nums[i - 3])
-            # print(f"prev3={prev3}, prev2={prev2}, prev1={prev1}")
+        for i in range(2, len(nums)):
+            prev2, prev1 = prev1, max(prev1, prev2 + nums[i - 2])
+            print(f"prev2={prev2}, prev1={prev1}")
         
-        return max(prev1 + nums[-1], prev2 + nums[-2], prev3 + nums[-3])
+        return max(prev1 + nums[-1], prev2 + nums[-2])
+
+
+                    # print(f"prev3={prev3}, prev2={prev2}, prev1={prev1}")
