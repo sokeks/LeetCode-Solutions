ee2a7e88ed2a113a609ad6aeef9ab9518c57df05
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-26 20:10:38 +0200

    Time: 0 ms (100%), Space: 19.3 MB (23.39%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.py
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -1,15 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
-        if len(nums) == 2: return max(nums[0], nums[1])
         prev2 = 0
         prev1 = 0
 
-        for i in range(2, len(nums)):
-            prev2, prev1 = prev1, max(prev1, prev2 + nums[i - 2])
-            print(f"prev2={prev2}, prev1={prev1}")
+        for i in range(len(nums)):
+            prev2, prev1 = prev1, max(prev1, prev2 + nums[i])
+            # print(f"prev2={prev2}, prev1={prev1}")
         
-        return max(prev1 + nums[-1], prev2 + nums[-2])
+        return prev1
 
 
                     # print(f"prev3={prev3}, prev2={prev2}, prev1={prev1}")
