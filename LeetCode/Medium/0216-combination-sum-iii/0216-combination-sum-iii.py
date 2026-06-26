6531cda669de6204f1f016a25e9d1606197d78fe
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-26 08:44:04 +0200

    Time: 3 ms (20.7%), Space: 19.4 MB (32.96%) - LeetHub

--- LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.py
+++ LeetCode/Medium/0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -5,15 +5,15 @@ class Item(NamedTuple):
     current_sum: int
 
 class Solution:
-    def _populate_stack_with_next_numbers(self, stack : Deque[Item], current: Item, target: int):
+    def _populate_stack_with_next_numbers(self, stack : Deque[Item], current: Item, target_k: int, target_sum: int):
         for i in range(current.number + 1, 10):
             new_sum = current.current_sum + i
-            if new_sum > target: break
+            if new_sum > target_sum: break
             stack.append(Item(number=i, position=current.position + 1, current_sum=new_sum))
 
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         stack = deque()
-        self._populate_stack_with_next_numbers(stack, Item(number=0, position=-1, current_sum=0), n)
+        self._populate_stack_with_next_numbers(stack, Item(number=0, position=-1, current_sum=0), k, n)
 
         combinations = []
         elements = [None] * k
@@ -23,7 +23,7 @@ class Solution:
             if item.position == k - 1:
                 if item.current_sum == n:
                     combinations.append(elements.copy())
-            else:
-                self._populate_stack_with_next_numbers(stack, item, n)
+                continue
+            self._populate_stack_with_next_numbers(stack, item, k, n)
 
         return combinations
