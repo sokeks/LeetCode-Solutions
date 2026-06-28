aef47c586df51498b77da109759826d4f22a7451
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-28 11:40:24 +0200

    Time: 31 ms (11.74%), Space: 19.1 MB (84.13%) - LeetHub

--- LeetCode/Easy/1025-divisor-game/1025-divisor-game.py
+++ LeetCode/Easy/1025-divisor-game/1025-divisor-game.py
@@ -4,8 +4,25 @@
 # 2. Having odd one has to pass to the second player even, as all odd divisors has to be odd, and odd_number_1 - odd_number_2 = even
 # 3. Having even, one can always pass to the second player odd, by choosing the divisor = 1
 # 4. By 2. & 3., someone starting with odd can be forced to always get back odd and keeping this will make one go eventally to 1, which by 1. looses
-# Therefore, if Alice starts with odd, she will eventually fail
+# Therefore, if Alice starts with odd, she will eventually fail. This pure analytical mathematical solution represented by n % 2 == 0
+#
+# However, if one wants to check a result using DP, see code below. In it, assuming n == 1 means always loose of Alice, we want to find if Alice
+# can give to Bob such a number, that will means his loosing. We do it by calculating all game results from start value == 2 up to n and return result for n
 
 class Solution:
+    # # Optimal O(1) calculation and memory solution
+    # def divisorGame(self, n: int) -> bool:
+    #     return n % 2 == 0
+    
+    # DP solution O(n^2) calculation and O(n) memory
     def divisorGame(self, n: int) -> bool:
-        return n % 2 == 0
+        isAliceWinning = [False] * (n + 1)
+        for i in range (2, n + 1):
+            for x in range (1, i):
+                if i % x != 0 or isAliceWinning[i - x]: continue
+                isAliceWinning[i] = True
+                break
+
+
+
+        return isAliceWinning[n]
