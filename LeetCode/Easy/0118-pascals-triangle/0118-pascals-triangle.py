906c9b9f550036d136f587e223725330ff464c4a
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-28 20:15:02 +0200

    Time: 0 ms (100%), Space: 19.3 MB (68.09%) - LeetHub

--- LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.py
+++ LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        rows = []
-        for r in range(numRows):
-            row = [1] * (r + 1)
-            for i in range(1, r):
-                row[i] = rows[r - 1][i - 1] + rows[r - 1][i]
-            rows.append(row)
+        triangle = [[1]]
+        for row in range(1, numRows):
+            left_shifted = [0] + triangle[row - 1]
+            right_shifted = triangle[row - 1] + [0]
+
+            triangle.append([a + b for a, b in zip(left_shifted, right_shifted, strict=True)])
         
-        return rows
-
-
-
-
-
-        
+        return triangle
