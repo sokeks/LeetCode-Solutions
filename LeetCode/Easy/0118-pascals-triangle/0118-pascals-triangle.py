ceada0f936692ace775234723adb3161b3a34e07
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-28 20:16:49 +0200

    Time: 2 ms (10.35%), Space: 19.2 MB (68.09%) - LeetHub

--- LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.py
+++ LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,6 +5,6 @@ class Solution:
             left_shifted = [0] + triangle[row - 1]
             right_shifted = triangle[row - 1] + [0]
 
-            triangle.append([a + b for a, b in zip(left_shifted, right_shifted, strict=True)])
+            triangle.append([a + b for a, b in zip(left_shifted, right_shifted)]) # no strict=True, as these are my own equal arrays
         
         return triangle
