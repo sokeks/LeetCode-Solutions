9ad630a97ff726cfb83d0fa2e8b1fcb6433b7a69
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-28 11:21:19 +0200

    Time: 0 ms (100%), Space: 19.1 MB (84.13%) - LeetHub

--- LeetCode/Easy/1025-divisor-game/1025-divisor-game.py
+++ LeetCode/Easy/1025-divisor-game/1025-divisor-game.py
@@ -1,3 +1,11 @@
+# Wrote all cases from 1 to 11 and found that on even Alice wins, while on odd Bob wins, however it's not enough...
+# Rules:
+# 1. Ending up at n = 1 means loosing
+# 2. Having odd one has to pass to the second player even, as all odd divisors has to be odd, and odd_number_1 - odd_number_2 = even
+# 3. Having even, one can always pass to the second player odd, by choosing the divisor = 1
+# 4. By 2. & 3., someone starting with odd can be forced to always get back odd and keeping this will make one go eventally to 1, which by 1. looses
+# Therefore, if Alice starts with odd, she will eventually fail
+
 class Solution:
     def divisorGame(self, n: int) -> bool:
         return n % 2 == 0
