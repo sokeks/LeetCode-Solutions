26caa776c324b57b7e7f57b1f402edd3593a87c2
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-29 09:00:05 +0200

    Time: 4 ms (64.42%), Space: 20.1 MB (94.29%) - LeetHub

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.py
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.py
@@ -3,7 +3,7 @@ class Solution:
         ans = [0] * (n + 1)
 
         for i in range(n + 1):
-            ans[i] = ans[i >> 1] + i % 2
+            ans[i] = ans[i >> 1] + (i & 0x01)
 
         return ans
         
