eba5cea676b4bc26046e0704b618f8d4eb4c7d67
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-29 08:22:18 +0200

    Time: 0 ms (100%), Space: 19.3 MB (57.57%) - LeetHub

--- LeetCode/Easy/0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ LeetCode/Easy/0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,21 +1,21 @@
 
 class Solution:
-    # # classic, DP solution
-    # def getRow(self, rowIndex: int) -> List[int]:
-    #     row = [1] * (rowIndex + 1)
-
-    #     for r in range(2, rowIndex + 1):
-    #         for c in range(r - 1, 0, -1):
-    #             row[c] += row[c - 1]
-            
-    #     return row
-
-    # binomial coefficient solution
+    # classic, DP solution
     def getRow(self, rowIndex: int) -> List[int]:
         row = [1] * (rowIndex + 1)
-        
-        for i in range(1, rowIndex + 1):
-            # Multiply first, then integer division to prevent truncation
-            row[i] = row[i - 1] * (rowIndex - i + 1) // i
+
+        for r in range(2, rowIndex + 1):
+            for c in range(r - 1, 0, -1):
+                row[c] += row[c - 1]
             
         return row
+
+    # # binomial coefficient solution
+    # def getRow(self, rowIndex: int) -> List[int]:
+    #     row = [1] * (rowIndex + 1)
+        
+    #     for i in range(1, rowIndex + 1):
+    #         # Multiply first, then integer division to prevent truncation
+    #         row[i] = row[i - 1] * (rowIndex - i + 1) // i
+            
+    #     return row
