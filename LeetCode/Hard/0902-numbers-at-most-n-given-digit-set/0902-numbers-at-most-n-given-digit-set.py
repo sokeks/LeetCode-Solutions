861adf6c990940cb29e2f1e3ff9e4dcb11d76aa3
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-30 18:30:59 +0200

    Time: 0 ms (100%), Space: 19.5 MB (27.34%) - LeetHub

--- LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
+++ LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
@@ -1,23 +1,23 @@
 class Solution:
-    def atMostNGivenDigitSetRec(self, digits: List[str], n: int, memory: dict[tuple[pos: int, tight: bool, is_started: bool], combinations: int], pos: int, tight: bool, is_started: bool) -> int:
-        if pos == len(str(n)): return 1
-        if (pos, tight, is_started) in memory: return memory[(pos, tight, is_started)]
-
-        limit = int(str(n)[pos]) if tight else 9
-
-        combinations = 0
-        for d in digits:
-            if int(d) > limit: continue
-            combinations += self.atMostNGivenDigitSetRec(digits, n, memory, pos + 1, tight and int(d) == limit, True)
-
-        if not is_started and pos < len(str(n)) - 1:
-            combinations += self.atMostNGivenDigitSetRec(digits, n, memory, pos + 1, False, False)
-
-        memory[(pos, tight, is_started)] = combinations
-
-        return combinations
-
-
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
-        memory = {}
-        return self.atMostNGivenDigitSetRec(digits, n, memory, 0, True, False)
+        n_str = str(n)
+        memory: dict[tuple[pos: int, tight: bool, is_started: bool], combinations: int] = {}
+        def atMostNGivenDigitSetRec(pos: int, tight: bool, is_started: bool) -> int:
+            if pos == len(str(n)): return 1 if is_started else 0
+            if (pos, tight, is_started) in memory: return memory[(pos, tight, is_started)]
+
+            limit = int(str(n)[pos]) if tight else 9
+
+            combinations = 0
+            for d in digits:
+                if int(d) > limit: continue
+                combinations += atMostNGivenDigitSetRec(pos + 1, tight and int(d) == limit, True)
+
+            if not is_started and pos < len(str(n)) - 1:
+                combinations += atMostNGivenDigitSetRec(pos + 1, False, False)
+
+            memory[(pos, tight, is_started)] = combinations
+
+            return combinations
+
+        return atMostNGivenDigitSetRec(0, True, False)
