553c5ae32d840567aaf6c41f3854f282cd639567
sokeks <131011732+sokeks@users.noreply.github.com>
2026-06-30 18:34:38 +0200

    Time: 4 ms (2.73%), Space: 19.6 MB (27.34%) - LeetHub

--- LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
+++ LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
@@ -1,19 +1,22 @@
 class Solution:
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
         n_str = str(n)
+        digits_int = [int(d) for d in digits]
         memory: dict[tuple[pos: int, tight: bool, is_started: bool], combinations: int] = {}
+
+
         def atMostNGivenDigitSetRec(pos: int, tight: bool, is_started: bool) -> int:
-            if pos == len(str(n)): return 1 if is_started else 0
+            if pos == len(n_str): return 1 if is_started else 0
             if (pos, tight, is_started) in memory: return memory[(pos, tight, is_started)]
 
-            limit = int(str(n)[pos]) if tight else 9
+            limit = int(n_str[pos]) if tight else 9
 
             combinations = 0
-            for d in digits:
-                if int(d) > limit: continue
-                combinations += atMostNGivenDigitSetRec(pos + 1, tight and int(d) == limit, True)
+            for d in digits_int:
+                if d > limit: continue
+                combinations += atMostNGivenDigitSetRec(pos + 1, tight and d == limit, True)
 
-            if not is_started and pos < len(str(n)) - 1:
+            if not is_started:
                 combinations += atMostNGivenDigitSetRec(pos + 1, False, False)
 
             memory[(pos, tight, is_started)] = combinations
