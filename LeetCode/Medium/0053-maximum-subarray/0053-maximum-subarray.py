209f5e27097ead430fd6d5d9cdf3a046f55d2473
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-01 17:11:21 +0200

    Time: 43 ms (33.73%), Space: 31.4 MB (43.63%) - LeetHub

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        prefix_sums_heap = [0]
-
+        smallest_prefix = 0
         largest_sum = float("-inf")
         current_prefix = 0
         for n in nums:
             current_prefix += n
-            # print(f"current_prefix={current_prefix}\tprefix_sums_heap[0]={prefix_sums_heap[0]}\tlargest_sum={largest_sum}")
-            largest_sum = max(current_prefix - prefix_sums_heap[0], largest_sum)
-            heapq.heappush(prefix_sums_heap, current_prefix)
+            largest_sum = max(current_prefix - smallest_prefix, largest_sum)
+            smallest_prefix = min(current_prefix, smallest_prefix)
 
         return largest_sum
