4588f774790fb2f77e1127429b08f5e882b202bc
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-01 10:10:35 +0200

    Time: 0 ms (100%), Space: 19.6 MB (27.06%) - LeetHub

--- LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
+++ LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
@@ -3,8 +3,8 @@ class Solution:
         n_str = str(n)
 
         @cache
-        def atMostNGivenDigitSetRec(pos: int, tight: bool, is_started: bool) -> int:
-            if pos == len(n_str): return 1 if is_started else 0
+        def atMostNGivenDigitSetRec(pos: int, tight: bool, was_first_digit_placed: bool) -> int:
+            if pos == len(n_str): return 1 if was_first_digit_placed else 0
 
             limit = n_str[pos] if tight else "9"
 
@@ -13,7 +13,7 @@ class Solution:
                 if d > limit: break
                 combinations += atMostNGivenDigitSetRec(pos + 1, tight and d == limit, True)
 
-            if not is_started:
+            if not was_first_digit_placed:
                 combinations += atMostNGivenDigitSetRec(pos + 1, False, False)
 
             return combinations
