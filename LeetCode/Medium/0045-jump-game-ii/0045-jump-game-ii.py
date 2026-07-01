105eb007a9ec5f90434eade1d0fe11faaa340fe0
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-01 16:42:29 +0200

    Time: 7 ms (53.77%), Space: 20 MB (66.13%) - LeetHub

--- LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.py
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.py
@@ -6,10 +6,13 @@ class Solution:
         next_reach = 0
         for i in range(len(nums) - 1):
             next_reach = max(next_reach, nums[i] + i)
+            if next_reach >= len(nums) - 1:
+                return jumps_count + 1
+
             if current_reach == i:
                 jumps_count += 1
                 current_reach = next_reach
-                if current_reach >= len(nums) - 1:
-                    return jumps_count
+            
+
 
         return jumps_count
