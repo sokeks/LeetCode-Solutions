5152d64610b4ea2db5b230ca43260af201ed52f0
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-01 10:03:23 +0200

    Time: 0 ms (100%), Space: 19.5 MB (27.06%) - LeetHub

--- LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
+++ LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
@@ -1,19 +1,17 @@
 class Solution:
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
         n_str = str(n)
-        digits_int = [int(d) for d in digits]
         memory: dict[tuple[pos: int, tight: bool, is_started: bool], combinations: int] = {}
-
 
         def atMostNGivenDigitSetRec(pos: int, tight: bool, is_started: bool) -> int:
             if pos == len(n_str): return 1 if is_started else 0
             if (pos, tight, is_started) in memory: return memory[(pos, tight, is_started)]
 
-            limit = int(n_str[pos]) if tight else 9
+            limit = n_str[pos] if tight else "9"
 
             combinations = 0
-            for d in digits_int:
-                if d > limit: continue
+            for d in digits:
+                if d > limit: break
                 combinations += atMostNGivenDigitSetRec(pos + 1, tight and d == limit, True)
 
             if not is_started:
