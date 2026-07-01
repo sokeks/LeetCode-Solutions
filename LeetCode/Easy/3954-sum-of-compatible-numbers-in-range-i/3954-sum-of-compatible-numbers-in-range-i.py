61047b3935ed6d8cc27e073a163ee9808219f951
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-01 13:51:36 +0200

    Time: 41 ms (5.04%), Space: 20.8 MB (5.03%) - LeetHub

--- LeetCode/Easy/3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.py
+++ LeetCode/Easy/3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.py
@@ -1,30 +1,61 @@
 class Solution:
+    # Below version is for maximized constaints, where the scope of x goes high above, what's given originally for this task. I
+    # have done it for the purposes of learning Digit DP approach. For the solution strictly related to this task's constraints
+    #
     def sumOfGoodIntegers(self, n: int, k: int) -> int:
         @cache
-        def sumOfGoodIntegersRec(bit_pos: int, tight: bool, limit: int) -> Tuple[int, int]:
-            if bit_pos == -1:
-                # print("return (0,1)")
-                return (0,1)
+        def sumOfIntegersRec(bit_pos: int) -> Tuple[suffixes_sum: int, suffixes_count: int]:
+            if bit_pos == -1: return (0, 1)
+            
+            suffixes_sum, suffixes_count = 0,0
+            for bit_value in (0, 1):
+                if ((n >> bit_pos) & (bit_value)): continue
 
-            current_bit_limit = ((limit >> bit_pos) & 0x01) if tight else 1
-            # print(f"bit_pos={bit_pos} ; (limit >> bit_pos) & 0x01)={((limit >> bit_pos) & 0x01)} current_bit_limit={current_bit_limit}")
+                tail_suffixes_sum, tail_suffixes_count = sumOfIntegersRec(bit_pos - 1)
 
-            sufixes_sum, sufixes_count = 0,0
+                suffixes_sum += (bit_value * (1 << bit_pos) * tail_suffixes_count) + tail_suffixes_sum
+                suffixes_count += tail_suffixes_count
+            
+            return (suffixes_sum, suffixes_count)
+
+        def sumOfGoodIntegersRec(bit_pos: int, limit: int) -> Tuple[int, int]:
+            if bit_pos == -1: return (0,1)
+
+            current_bit_limit = (limit >> bit_pos) & 0x01
+
+            suffixes_sum, suffixes_count = 0,0
             for bit_value in (0, 1):
                 if bit_value > current_bit_limit or ((n >> bit_pos) & (bit_value)): continue
 
-                # padding = "x" * bit_pos
-                # print(f"{bits}{bit_value}{padding}")
-                lower_sufixes_sum, lower_sufixes_count = sumOfGoodIntegersRec(bit_pos - 1, tight and current_bit_limit == bit_value, limit)
-                sufixes_sum += (bit_value * (1 << bit_pos) * lower_sufixes_count) + lower_sufixes_sum
-                sufixes_count += lower_sufixes_count
-                # print(f"{bits}{bit_value}{padding}: sufixes_sum={sufixes_sum} ; sufixes_count={sufixes_count}; limit={limit:b}")
+                tail_suffixes_sum, tail_suffixes_count = sumOfGoodIntegersRec(bit_pos - 1, limit) if bit_value == current_bit_limit else sumOfIntegersRec(bit_pos - 1)
+                
+                suffixes_sum += (bit_value * (1 << bit_pos) * tail_suffixes_count) + tail_suffixes_sum
+                suffixes_count += tail_suffixes_count
 
-            return sufixes_sum, sufixes_count
+            return suffixes_sum, suffixes_count
 
-        # return sumOfGoodIntegersRec(2, True, 5, "")
-        return sumOfGoodIntegersRec(60, True, k + n)[0] - sumOfGoodIntegersRec(60, True, max(1, n - k) - 1)[0]
+        return sumOfGoodIntegersRec(60, k + n)[0] - sumOfGoodIntegersRec(60, max(1, n - k) - 1)[0]
 
+        # def sumOfGoodIntegersRec(bit_pos: int, tight: bool, limit: int) -> Tuple[int, int]:
+        #     if bit_pos == -1:
+        #         return (0,1)
 
+        #     current_bit_limit = ((limit >> bit_pos) & 0x01) if tight else 1
+
+        #     suffixes_sum, suffixes_count = 0,0
+        #     for bit_value in (0, 1):
+        #         if bit_value > current_bit_limit or ((n >> bit_pos) & (bit_value)): continue
+
+        #         tail_suffixes_sum, tail_suffixes_count = sumOfGoodIntegersRec(bit_pos - 1, tight and current_bit_limit == bit_value, limit)
+                
+        #         suffixes_sum += (bit_value * (1 << bit_pos) * tail_suffixes_count) + tail_suffixes_sum
+        #         suffixes_count += tail_suffixes_count
+
+        #     return suffixes_sum, suffixes_count
+
+        # return sumOfGoodIntegersRec(60, True, k + n)[0] - sumOfGoodIntegersRec(60, True, max(1, n - k) - 1)[0]
+
+    # Below version is enough for given constaints and shuold be presented as the default solution for this particular LC task
+    #
     # def sumOfGoodIntegers(self, n: int, k: int) -> int:
     #     return sum(x for x in range(max(1, n - k), k + n + 1) if n & x == 0)
