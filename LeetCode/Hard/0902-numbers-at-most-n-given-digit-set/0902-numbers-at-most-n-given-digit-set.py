716a9105015ef92d20848c1470f7ed2d94ff535e
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-01 10:11:24 +0200

    Time: 3 ms (13.33%), Space: 19.7 MB (10.2%) - LeetHub

--- LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
+++ LeetCode/Hard/0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.py
@@ -8,14 +8,14 @@ class Solution:
 
             limit = n_str[pos] if tight else "9"
 
-            combinations = 0
+            positive_ints_count = 0
             for d in digits:
                 if d > limit: break
-                combinations += atMostNGivenDigitSetRec(pos + 1, tight and d == limit, True)
+                positive_ints_count += atMostNGivenDigitSetRec(pos + 1, tight and d == limit, True)
 
             if not was_first_digit_placed:
-                combinations += atMostNGivenDigitSetRec(pos + 1, False, False)
+                positive_ints_count += atMostNGivenDigitSetRec(pos + 1, False, False)
 
-            return combinations
+            return positive_ints_count
 
         return atMostNGivenDigitSetRec(0, True, False)
