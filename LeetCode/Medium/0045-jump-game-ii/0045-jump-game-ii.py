2a46bc7ef31fe5531e0020bb14c0fb007243a13f
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-01 16:33:53 +0200

    Time: 4 ms (70.1%), Space: 20.3 MB (15.89%) - LeetHub

---
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        jumps_count = 0
+        current_reach = 0
+        next_reach = 0
+        for i in range(len(nums) - 1):
+            next_reach = max(next_reach, nums[i] + i)
+            if current_reach == i:
+                jumps_count += 1
+                current_reach = next_reach
+
+        return jumps_count
+
+
+        
