ccd386ba4ce40810920433d7f9000fdadaf7d7e3
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-02 08:57:38 +0200

    Time: 0 ms (100%), Space: 19.4 MB (41.98%) - LeetHub

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.py
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.py
@@ -1,12 +1,15 @@
 class Solution:
-    # Based on iteraion - less efficient comparing to the combinatorics below
+    # Based on iteraion - less efficient comparing to the combinatorics below, however showing DP approach with optimized table to one unqiue_paths_row only.
+    # For the optimization - please note, that we have only 1 way to get to first column (by going, obviously, down), and for other columns, the
+    # way to get there is either from the left or from the top. See comments inside.
     def uniquePaths(self, m: int, n: int) -> int:
-        leading_path_combinations_count = [0] * n
-        leading_path_combinations_count[0] = 1
-        for i in range(m):
+        # there is only 1 way to get to the top row
+        unqiue_paths_row = [1] * n
+        for _ in range(1, m):
             for j in range(1, n):
-                leading_path_combinations_count[j] = leading_path_combinations_count[j] + leading_path_combinations_count[j - 1]
-        return leading_path_combinations_count[n - 1]
+                # j - paths from the top, (j - 1) - paths from the left 
+                unqiue_paths_row[j] += unqiue_paths_row[j - 1]
+        return unqiue_paths_row[-1]
 
 
     # Based on combinatorics - we know we need to make m - 1 steps down and n - 1 steps right, so it's about permutation of those.
