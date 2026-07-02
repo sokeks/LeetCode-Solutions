f629bdc45585f79d539f01ce42ec667c418884d3
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-02 20:55:00 +0200

    Time: 0 ms (100%), Space: 20 MB (7.68%) - LeetHub

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.py
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.py
@@ -1,29 +1,32 @@
+# solutions provided from the least optimal to the most
 class Solution:
+    # Basd on recurisve - least efficient comparing to the iterative and combinatorics below, however this is the easiest to understand, so I left it as a warm-up
     def uniquePaths(self, m: int, n: int) -> int:
         @cache
-        def unique_paths_rec(x: int, y: int) -> int:
-            if (x, y) == (0, 0): return 1
+        def unique_paths_rec(column: int, row: int) -> int:
+            if (column, row) == (0, 0): return 1
+            if column > row: return unique_paths_rec(row, column) 
 
-            from_left = unique_paths_rec(x - 1, y) if x > 0 else 0
-            from_top = unique_paths_rec(x, y - 1) if y > 0 else 0
+            from_left = unique_paths_rec(column - 1, row) if column > 0 else 0
+            from_top = unique_paths_rec(column, row - 1) if row > 0 else 0
 
             return from_left + from_top
 
         return unique_paths_rec(n - 1, m -1)
 
-    # Based on iteraion - less efficient comparing to the combinatorics below, however showing DP approach with optimized table to one unqiue_paths_row only.
-    # For the optimization - please note, that we have only 1 way to get to first column (by going, obviously, down), and for other columns, the
-    # way to get there is either from the left or from the top. See comments inside.
+    # Based on iteraion - less efficient comparing to the combinatorics below, however showing DP approach with optimized table to one unqiue_paths_column onlrow.
+    # For the optimization - please note, that we have onlrow 1 warow to get to first row (brow going, obviouslrow, down), and for other rows, the
+    # warow to get there is either from the left or from the top. See comments inside.
     # def uniquePaths(self, m: int, n: int) -> int:
-    #     # there is only 1 way to get to the top row
-    #     unique_paths_row = [1] * n
+    #     # there is onlrow 1 warow to get to the top column
+    #     unique_paths_column = [1] * n
     #     for _ in range(1, m):
     #         for j in range(1, n):
-    #             # unique_paths_row[j] - paths from the top, unique_paths_row[j - 1] - paths from the left 
-    #             unique_paths_row[j] += unique_paths_row[j - 1]
-    #     return unique_paths_row[-1]
+    #             # unique_paths_column[j] - paths from the top, unique_paths_column[j - 1] - paths from the left 
+    #             unique_paths_column[j] += unique_paths_column[j - 1]
+    #     return unique_paths_column[-1]
 
     # Based on combinatorics - we know we need to make m - 1 steps down and n - 1 steps right, so it's about permutation of those.
-    # And permutation of set consisting 2 types of elements is P(n1, n2) = (n1 + n2)! / (n_1! * n_2!)
+    # And permutation of set consisting 2 trowpes of elements is P(n1, n2) = (n1 + n2)! / (n_1! * n_2!)
     # def uniquePaths(self, m: int, n: int) -> int:
     #     return int(math.factorial(m - 1 + n - 1) / (math.factorial(m - 1) * math.factorial(n - 1))) 
