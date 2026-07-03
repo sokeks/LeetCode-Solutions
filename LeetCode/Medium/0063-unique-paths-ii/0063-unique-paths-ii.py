406cac31e0872f49a1bcb4ab4e996c1c085227b1
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-03 07:43:04 +0200

    Time: 4 ms (8.12%), Space: 19.9 MB (5.04%) - LeetHub

--- LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,28 +1,36 @@
 class Solution:
-    # def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-    #     def uniquePathsWithObstaclesRec(row: int, col: int):
-
-
-
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        def get_first_row_unique_paths(row):
-            was_obstacle = False
-            for square in row:
-                if square == 1 or was_obstacle:
-                    was_obstacle = True
-                    yield 0
-                else: yield 1
+        @cache
+        def uniquePathsWithObstaclesRec(row: int, col: int) -> int:
+            if obstacleGrid[row][col] == 1: return 0
+            if (row, col) == (0, 0): return 1
+
+            return ((uniquePathsWithObstaclesRec(row - 1, col) if row > 0 else 0)
+                    + (uniquePathsWithObstaclesRec(row, col - 1) if col > 0 else 0))
+
+        return  uniquePathsWithObstaclesRec(len(obstacleGrid) - 1, len(obstacleGrid[0]) - 1)
+
+
+
+    # def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    #     def get_first_row_unique_paths(row):
+    #         was_obstacle = False
+    #         for square in row:
+    #             if square == 1 or was_obstacle:
+    #                 was_obstacle = True
+    #                 yield 0
+    #             else: yield 1
         
-        unique_paths_row = list(get_first_row_unique_paths(obstacleGrid[0]))
+    #     unique_paths_row = list(get_first_row_unique_paths(obstacleGrid[0]))
 
-        columns_count = len(obstacleGrid[0])
-        for i in range(1, len(obstacleGrid)):
-            if obstacleGrid[i][0] == 1:
-                unique_paths_row[0] = 0
-            for j in range(1, columns_count):
-                if obstacleGrid[i][j] == 1:
-                    unique_paths_row[j] = 0
-                else:
-                    unique_paths_row[j] += unique_paths_row[j - 1]
+    #     columns_count = len(obstacleGrid[0])
+    #     for i in range(1, len(obstacleGrid)):
+    #         if obstacleGrid[i][0] == 1:
+    #             unique_paths_row[0] = 0
+    #         for j in range(1, columns_count):
+    #             if obstacleGrid[i][j] == 1:
+    #                 unique_paths_row[j] = 0
+    #             else:
+    #                 unique_paths_row[j] += unique_paths_row[j - 1]
 
-        return unique_paths_row[-1]
+    #     return unique_paths_row[-1]
