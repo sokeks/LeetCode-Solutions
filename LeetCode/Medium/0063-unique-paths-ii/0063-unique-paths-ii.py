6d8d80aee3c7cb23af12263394654aa72fdf2a27
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-03 07:24:46 +0200

    Time: 0 ms (100%), Space: 19.5 MB (17.57%) - LeetHub

--- LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -6,8 +6,8 @@ class Solution:
 
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         def get_first_row_unique_paths(row):
-
-
+            # adding additional column to have conditional-less main loop
+            yield 0
 
             was_obstacle = False
             for square in row:
@@ -17,14 +17,15 @@ class Solution:
                 else: yield 1
         
         unique_paths_row = list(get_first_row_unique_paths(obstacleGrid[0]))
+        print(unique_paths_row)
 
-        columns_count = len(obstacleGrid[0])
+        columns_count = len(unique_paths_row)
         for i in range(1, len(obstacleGrid)):
-            for j in range(columns_count):
-                if obstacleGrid[i][j] == 1:
+            for j in range(1, columns_count):
+                if obstacleGrid[i][j - 1] == 1:
                     unique_paths_row[j] = 0
                 else:
-                    from_left = unique_paths_row[j - 1] if j > 0 else 0
+                    from_left = unique_paths_row[j - 1]
                     from_top = unique_paths_row[j]
                     unique_paths_row[j] = from_left + from_top
 
