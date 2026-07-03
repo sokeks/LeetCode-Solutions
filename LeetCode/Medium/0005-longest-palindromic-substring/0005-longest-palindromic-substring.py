cf2fa71de8ef850cc58939457316e9de1c27269f
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-03 10:33:58 +0200

    Time: 439 ms (35.62%), Space: 19.3 MB (69.24%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,19 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        def get_longest_palindrome(start: int, left_modifier: int):
+            offset = 0
+            while ((start - (offset + 1 - left_modifier)) >= 0 and (start + (offset + 1)) < len(s)
+                    and s[start - (offset + 1 - left_modifier)] == s[start + (offset + 1)]): offset += 1
+            
+            palindrome_start = start - (offset - left_modifier)
+            palindrome_end = start + offset + 1
+            return palindrome_start, palindrome_end, palindrome_end - palindrome_start
+        
         substring_start = 0
         substring_end = 1
+        substring_len = substring_end - substring_start
 
         for i in range(len(s)):
-            j = 0
-            while (i - (j + 1)) >= 0 and (i + (j + 1)) < len(s) and s[i - (j + 1)] == s[i + (j + 1)]: j += 1
-            with_mid_len = 2 * j + 1
+            with_mid_start, with_mid_end, with_mid_len = get_longest_palindrome(i, 0)
+            without_mid_start, without_mid_end, without_mid_len = get_longest_palindrome(i, 1)
 
-            k = 0
-            while (i - k) >= 0 and (i + (k + 1)) < len(s) and s[i - k] == s[i + (k + 1)]: k += 1
-            without_mid_len = 2 * k
-            
-            substring_len = substring_end - substring_start
             if substring_len < with_mid_len or substring_len < without_mid_len:
-                substring_start, substring_end = (i - j, i + j + 1) if with_mid_len > without_mid_len else (i - (k - 1), i + k + 1)
+                substring_start, substring_end = (with_mid_start, with_mid_end) if with_mid_len > without_mid_len else (without_mid_start, without_mid_end)
+                substring_len = substring_end - substring_start
+
+            # if substring_len > len(s) - i: break
 
         return s[substring_start:substring_end]
