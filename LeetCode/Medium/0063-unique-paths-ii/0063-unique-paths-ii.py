60d471ad046395e6d8ffdfc88680819e96fc0556
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-03 07:16:36 +0200

    Time: 0 ms (100%), Space: 19.4 MB (32.77%) - LeetHub

--- LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,4 +1,9 @@
 class Solution:
+    # def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    #     def uniquePathsWithObstaclesRec(row: int, col: int):
+
+
+
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         def get_first_row_unique_paths(row):
             was_obstacle = False
@@ -9,12 +14,15 @@ class Solution:
                 else: yield 1
         
         unique_paths_row = list(get_first_row_unique_paths(obstacleGrid[0]))
-        print(unique_paths_row)
 
+        columns_count = len(obstacleGrid[0])
         for i in range(1, len(obstacleGrid)):
-            for j in range(len(obstacleGrid[0])):
-                from_left = unique_paths_row[j - 1] if j > 0 else 0
-                unique_paths_row[j] = 0 if obstacleGrid[i][j] == 1 else from_left + unique_paths_row[j]
+            for j in range(columns_count):
+                if obstacleGrid[i][j] == 1:
+                    unique_paths_row[j] = 0
+                else:
+                    from_left = unique_paths_row[j - 1] if j > 0 else 0
+                    from_top = unique_paths_row[j]
+                    unique_paths_row[j] = from_left + from_top
 
         return unique_paths_row[-1]
-        
