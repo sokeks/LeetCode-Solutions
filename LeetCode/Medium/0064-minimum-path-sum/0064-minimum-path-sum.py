674ca9453814e64cdf8cdd213afe30607f9cdb2c
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-03 08:25:06 +0200

    Time: 38 ms (13.73%), Space: 32.6 MB (5.05%) - LeetHub

--- LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.py
+++ LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,12 +1,22 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        min_path_sum_row = list(accumulate(grid[0]))
+        cost_row = list(accumulate(grid[0]))
 
-        min_path_sum_row_len = len(min_path_sum_row)
+        cost_row_len = len(cost_row)
         for row in range(1, len(grid)):
-            min_path_sum_row[0] += grid[row][0]
-            for col in range(1, min_path_sum_row_len):
-                min_path_sum_row[col] = grid[row][col] + min(min_path_sum_row[col], min_path_sum_row[col - 1])
+            cost_row[0] += grid[row][0]
+            for col in range(1, cost_row_len):
+                cost_row[col] = grid[row][col] + min(cost_row[col], cost_row[col - 1])
         
-        return min_path_sum_row[-1]
+        return cost_row[-1]
+
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        @cache
+        def minPathSum(row: int, col: int) -> int:
+            if row < 0 or col < 0: return sys.maxsize
+            if (row, col) == (0, 0): return grid[row][col]
+
+            return  grid[row][col] + min(minPathSum(row - 1, col), minPathSum(row, col - 1))
+        
+        return minPathSum(len(grid) - 1, len(grid[0]) - 1)
         
