128a174f3dac425be0e163db09b432efe033e374
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-05 13:41:04 +0200

    Time: 3 ms (80.33%), Space: 19.3 MB (57.56%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,16 +1,30 @@
 class Solution:
+    _ROMAN_VALUES_TO_DEC = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
     def romanToInt(self, s: str) -> int:
-        mapping = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
-
         integer = 0
-        previous_char_val = mapping["M"]
+        
+        previous_char_val = 0
+        for c in reversed(s):
+            c_int = self._ROMAN_VALUES_TO_DEC[c]
+            if c_int < previous_char_val:
+                integer -= c_int
+            else:
+                integer += c_int
+            previous_char_val = c_int
+
+        return integer
+
+
+
+
+        previous_char_val = self._ROMAN_VALUES_TO_DEC["M"]
         for c in s:
-            c_int = mapping[c]
+            c_int = self._ROMAN_VALUES_TO_DEC[c]
             integer += c_int
             if c_int > previous_char_val:
                 integer -= (2 * previous_char_val)
         
             previous_char_val = c_int
-            
+
         return integer
         
