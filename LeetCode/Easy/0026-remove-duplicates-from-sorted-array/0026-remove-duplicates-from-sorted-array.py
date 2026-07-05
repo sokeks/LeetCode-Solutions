a7bf45044c0a9cbd8b14d3e90892bb31a9aca179
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-05 16:34:10 +0200

    Time: 4 ms (23.88%), Space: 20.5 MB (78.16%) - LeetHub

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,17 +1,22 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        write_idx = -1
-        previous = nums[0]
-        duplicates_count = 0
+        write_idx = 1
+
+
         for read_idx in range(1, len(nums)):
             if nums[read_idx] != nums[read_idx - 1]:
-                if write_idx != -1:
-                    nums[write_idx] = nums[read_idx]
-                    write_idx += 1
-            else:
-                duplicates_count += 1
-                if write_idx == -1:
-                    write_idx = read_idx
-        
-        return len(nums) - duplicates_count
+                nums[write_idx] = nums[read_idx]
+                write_idx += 1
 
+
+
+            # if nums[read_idx] != nums[read_idx - 1]:
+            #     if write_idx != -1:
+            #         nums[write_idx] = nums[read_idx]
+            #         write_idx += 1
+            # else:
+            #     duplicates_count += 1
+            #     if write_idx == -1:
+            #         write_idx = read_idx
+        
+        return write_idx
