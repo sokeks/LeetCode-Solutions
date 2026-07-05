bb60a5a582d51a011fdd8033010c90b903bcf078
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-05 12:54:25 +0200

    Time: 11 ms (36.62%), Space: 19.3 MB (17.71%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x == 0: return True
-        if x < 0 or x % 10 == 0: return False
+        if x == 0:
+            return True
+        if x < 0 or x % 10 == 0:
+            return False
 
         x_reversed = 0
 
