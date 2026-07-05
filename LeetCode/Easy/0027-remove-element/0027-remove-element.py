89617eb80285b4157db0c1b75b069104b26947df
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-05 20:57:44 +0200

    Time: 0 ms (100%), Space: 19.2 MB (56.35%) - LeetHub

--- LeetCode/Easy/0027-remove-element/0027-remove-element.py
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.py
@@ -1,5 +1,19 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # solution with 2 pointers, both starting from the beginning - less memory writes, but destroys cache locality
+        # which is especially harmful for big arrays
+        write_ptr = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[write_ptr] = nums[i]
+                write_ptr += 1
+
+        return write_ptr
+
+
+
+        # solution with 2 pointers, but the second pointer starting from the end - less memory writes, but destroys cache locality
+        # which is especially harmful for big arrays
         garbage_ptr = len(nums) - 1
         read_ptr = 0
 
@@ -9,8 +23,5 @@ class Solution:
                 garbage_ptr -= 1
             else:
                 read_ptr += 1
-
-                        
+ 
         return read_ptr
-
-        
