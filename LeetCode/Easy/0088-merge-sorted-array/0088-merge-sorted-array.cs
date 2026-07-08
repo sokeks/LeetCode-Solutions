6f2717132ad73957924b565cb82a4ab3a9eaf9f7
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-08 08:52:41 +0200

    Time: 0 ms (100%), Space: 46.7 MB (73.39%) - LeetHub

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cs
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -1,30 +1,37 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
         var r = m + n - 1;
-        m--;
-        n--;
-        while (n >= 0 && m >= 0)
+        var i1 = m - 1;
+        var i2 = n - 1;
+        while (i1 >= 0 && i2 >= 0)
         {
-            if (nums1[m] > nums2[n])
+            if (nums1[i1] > nums2[i2])
             {
-                nums1[r--] = nums1[m--];
+                nums1[r--] = nums1[i1--];
             }
             else
             {
-                nums1[r--] = nums2[n--];
+                nums1[r--] = nums2[i2--];
             }
         }
 
-        while (n >= 0) nums1[r--] = nums2[n--];
-
-
-        // while (n >= 0)
-        // {
-        //     var result = m >= 0 && nums1[m] > nums2[n]
-        //         ? nums1[m--]
-        //         : nums2[n--];
-        //     nums1[r] = result;
-        //     r--;
-        // }
+        while (i2 >= 0) nums1[r--] = nums2[i2--];
     }
+
+    // Previous version, less readable and with arguments modifications, both in value and semantically (from count to index), better not to do that
+    public void MergeOld(int[] nums1, int m, int[] nums2, int n) {
+        var r = m + n - 1;
+        m--;
+        n--;
+
+        while (n >= 0)
+        {
+            var result = m >= 0 && nums1[m] > nums2[n]
+                ? nums1[m--]
+                : nums2[n--];
+            nums1[r] = result;
+            r--;
+        }
+    }
+
 }
