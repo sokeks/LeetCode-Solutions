f6c6ff5d255a8c1a8638ad47117345856068e295
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-08 08:29:39 +0200

    Time: 0 ms (100%), Space: 47.2 MB (28.61%) - LeetHub

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cs
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -3,13 +3,28 @@ public class Solution {
         var r = m + n - 1;
         m--;
         n--;
-        while (n >= 0)
+        while (n >= 0 && m >= 0)
         {
-            var result = m >= 0 && nums1[m] > nums2[n]
-                ? nums1[m--]
-                : nums2[n--];
-            nums1[r] = result;
-            r--;
+            if (nums1[m] > nums2[n])
+            {
+                nums1[r--] = nums1[m--];
+            }
+            else
+            {
+                nums1[r--] = nums2[n--];
+            }
         }
+
+        while (n >= 0) nums1[r--] = nums2[n--];
+
+
+        // while (n >= 0)
+        // {
+        //     var result = m >= 0 && nums1[m] > nums2[n]
+        //         ? nums1[m--]
+        //         : nums2[n--];
+        //     nums1[r] = result;
+        //     r--;
+        // }
     }
 }
