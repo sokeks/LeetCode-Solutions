65884e2dd8087685802089f21573130063806efb
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-08 19:38:40 +0200

    Time: 51 ms (25.55%), Space: 19.1 MB (64.03%) - LeetHub

--- LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.py
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.py
@@ -1,15 +1,10 @@
 class Solution:
-    _SIGNED_INT_BIT_COUNT = 31
     def reverseBits(self, n: int) -> int:
         reversed_n = 0
-        bits_count = 0
 
-        while n:
+        for _ in range(31):
             reversed_n |= n & 1
             reversed_n <<= 1
             n >>= 1
-            bits_count += 1
-
-        reversed_n <<= (self._SIGNED_INT_BIT_COUNT - bits_count)
         
         return reversed_n
