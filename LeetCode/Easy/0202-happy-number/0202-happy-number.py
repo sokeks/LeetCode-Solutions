e70f7bd76a2d2aa02c928f596c578cc405d7ab36
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-09 08:07:56 +0200

    Time: 3 ms (35.39%), Space: 19.3 MB (61%) - LeetHub

--- LeetCode/Easy/0202-happy-number/0202-happy-number.py
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.py
@@ -1,5 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        def generate_next_number(n: int):
+            return sum(int(x) ** 2 for x in str(n))
+            # result = 0
+            # while n:
+            #     n, remainder = divmod(n, 10)
+            #     result += (remainder ** 2)
+
+            # return result        
+                
+
         occurred = set()
 
         while n != 1:
@@ -7,12 +17,7 @@ class Solution:
                 return False
             
             occurred.add(n)
+            n = generate_next_number(n)
             
-            result = 0
-            while n:
-                result += ((n % 10) ** 2)
-                n //= 10
-            
-            n = result
         
         return True
