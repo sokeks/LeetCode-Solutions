68419e8bf0baf15310c912d2f26d218a8c05ef99
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-09 09:34:06 +0200

    Time: 1 ms (95.83%), Space: 19.2 MB (45.99%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,9 +1,8 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        ASCII_COUNT = 255
+        ASCII_COUNT = 128
         s_to_t_map = ['\0'] * ASCII_COUNT
         s_to_t_exist = [False] * ASCII_COUNT
-        t_to_s_map = ['\0'] * ASCII_COUNT
         t_to_s_exist = [False] * ASCII_COUNT
 
         for (ac, tc) in zip(s, t):
@@ -16,6 +15,5 @@ class Solution:
                 s_to_t_exist[ord(ac)] = True
                 s_to_t_map[ord(ac)] = tc
                 t_to_s_exist[ord(tc)] = True
-
 
         return True
