6eba235f9f67d4fc1da6a428f8402983ec15da17
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-13 08:45:02 +0200

    Time: 0 ms (100%), Space: 19.5 MB (16.44%) - LeetHub

--- LeetCode/Easy/0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ LeetCode/Easy/0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,3 +1,38 @@
+# class MyStack:
+
+#     def __init__(self):
+#         self.left = Deque()
+#         self.right = Deque()
+#         self.top_x = None
+
+#     def push(self, x: int) -> None:
+#         self.top_x = x
+#         if self.left:
+#             self.left.append(x)
+#         else:
+#             self.right.append(x)
+
+#     def pop(self) -> int:
+#         outbound, inbound = (self.left, self.right) if self.left else (self.right, self.left)
+#         for _ in range(len(outbound) - 2):
+#             inbound.append(outbound.popleft())
+
+#         if len(outbound) == 2:
+#             self.top_x = outbound.popleft()
+#             inbound.append(self.top_x)
+#         else:
+#             self.top_x = None
+        
+#         return outbound.popleft()
+
+#     def top(self) -> int:
+#         return self.top_x
+        
+
+#     def empty(self) -> bool:
+#         return not self.left and not self.right
+
+
 class MyStack:
 
     def __init__(self):
@@ -13,17 +48,12 @@ class MyStack:
             self.right.append(x)
 
     def pop(self) -> int:
-        outbound, inband = (self.left, self.right) if self.left else (self.right, self.left)
-        print(len(outbound) - 2)
-        for _ in range(len(outbound) - 2):
-            inband.append(outbound.popleft())
-
-        if len(outbound) == 2:
+        outbound, inbound = (self.left, self.right) if self.left else (self.right, self.left)
+        self.top_x = None
+        while len(outbound) > 1:
             self.top_x = outbound.popleft()
-            inband.append(self.top_x)
-        else:
-            self.top_x = None
-        
+            inbound.append(self.top_x)
+
         return outbound.popleft()
 
     def top(self) -> int:
