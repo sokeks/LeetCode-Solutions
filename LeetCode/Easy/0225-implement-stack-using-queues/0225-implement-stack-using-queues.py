647a22c15377611f8e1793c7bdacf8ba7408b918
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-13 08:50:57 +0200

    Time: 0 ms (100%), Space: 19.5 MB (16.44%) - LeetHub

--- LeetCode/Easy/0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ LeetCode/Easy/0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,5 +1,31 @@
-# class MyStack:
+class MyStack:
 
+    def __init__(self):
+        self.queue = Deque()
+
+    def push(self, x: int) -> None:
+        self.top_x = x
+        self.queue.append(x)
+
+
+    def pop(self) -> int:
+        self.top_x = None
+        for _ in range(len(self.queue) - 1):
+            self.top_x = self.queue.popleft()
+            self.queue.append(self.top_x)
+        
+        return self.queue.popleft()
+
+
+    def top(self) -> int:
+        return self.top_x
+        
+
+    def empty(self) -> bool:
+        return not self.queue
+
+# 2-queues version - before follow-up
+# class MyStack:
 #     def __init__(self):
 #         self.left = Deque()
 #         self.right = Deque()
@@ -14,15 +40,11 @@
 
 #     def pop(self) -> int:
 #         outbound, inbound = (self.left, self.right) if self.left else (self.right, self.left)
-#         for _ in range(len(outbound) - 2):
-#             inbound.append(outbound.popleft())
-
-#         if len(outbound) == 2:
+#         self.top_x = None
+#         while len(outbound) > 1:
 #             self.top_x = outbound.popleft()
 #             inbound.append(self.top_x)
-#         else:
-#             self.top_x = None
-        
+
 #         return outbound.popleft()
 
 #     def top(self) -> int:
@@ -31,37 +53,6 @@
 
 #     def empty(self) -> bool:
 #         return not self.left and not self.right
-
-
-class MyStack:
-
-    def __init__(self):
-        self.left = Deque()
-        self.right = Deque()
-        self.top_x = None
-
-    def push(self, x: int) -> None:
-        self.top_x = x
-        if self.left:
-            self.left.append(x)
-        else:
-            self.right.append(x)
-
-    def pop(self) -> int:
-        outbound, inbound = (self.left, self.right) if self.left else (self.right, self.left)
-        self.top_x = None
-        while len(outbound) > 1:
-            self.top_x = outbound.popleft()
-            inbound.append(self.top_x)
-
-        return outbound.popleft()
-
-    def top(self) -> int:
-        return self.top_x
-        
-
-    def empty(self) -> bool:
-        return not self.left and not self.right
 
 
 # Your MyStack object will be instantiated and called as such:
