0060bea1e421e4e29eb3498981eca459e28adfbd
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-13 08:02:25 +0200

    Time: 0 ms (100%), Space: 24 MB (18.05%) - LeetHub

--- LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ LeetCode/Easy/0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,15 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def countNodes(self, root: Optional[TreeNode]) -> int:
-        def find_left_depth(root: TreeNode) -> int:
-            depth = 0
-            current = root
-            while current:
-                depth += 1
-                current = current.left
-            return depth
+    # recursive version - optimal one, due to certainty, it's not a skewed tree, but complete - so
+    # depth -> O(log N)
+    # def countNodes(self, root: Optional[TreeNode]) -> int:
+    #     def find_left_depth(root: TreeNode) -> int:
+    #         depth = 0
+    #         current = root
+    #         while current:
+    #             depth += 1
+    #             current = current.left
+    #         return depth
 
+    #     def find_right_depth(root: TreeNode) -> int:
+    #         depth = 0
+    #         current = root
+    #         while current:
+    #             depth += 1
+    #             current = current.right
+    #         return depth
+        
+    #     if not root:
+    #         return 0
+
+    #     lh = find_left_depth(root)
+    #     rh = find_right_depth(root)
+
+    #     if lh == rh:
+    #         return (1 << lh) - 1
+        
+    #     return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+    def countNodes(self, root: Optional[TreeNode]) -> int:
         def find_right_depth(root: TreeNode) -> int:
             depth = 0
             current = root
@@ -21,62 +43,35 @@ class Solution:
                 depth += 1
                 current = current.right
             return depth
+
+        if not root: return 0
         
-        if not root:
-            return 0
+        tree_depth = find_right_depth(root)
+        print(tree_depth)
+        nodes_sum = (1 << tree_depth) - 1
 
-        lh = find_left_depth(root)
-        rh = find_right_depth(root)
+        left = 0
+        right = 1 << tree_depth
 
-        if lh == rh:
-            return (1 << lh) - 1
-        
-        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        def does_node_exist(node: TreeNode, path: int):
+            for i in range((tree_depth - 1), -1, -1):
+                step = (path >> i) & 1
+                print(f"step={step} i={i}")
+                # path, reminder = divmod(path, 2)
+                node = node.left if not step else node.right
+            print(bool(node))
+            return bool(node)
 
+        while left < right:
+            mid = (left + right) // 2
+            print(f"mid={mid}")
 
-
-        # if not root: return 0
-
-        # def find_right_depth(root: TreeNode) -> int:
-        #     depth = 0
-        #     current = root
-        #     while current:
-        #         depth += 1
-        #         current = current.right
-        #     return depth
-
-        # def get_if_exist_child_nodes(node: TreeNode, path: int) -> Tuple[bool, bool]:
-        #     while path:
-        #         path, reminder = divmod(path, 2)
-        #         node = node.right if reminder else node.left
-
-        #     return (bool(node.left), bool(node.right))
-
-        # tree_depth = find_right_depth(root)
-        # print(tree_depth)
-        # nodes_sum = (1 - 2**tree_depth) // (1 - 2)
-        # print(nodes_sum)
-        
-        # left = 0
-        # right = 2 ** (tree_depth - 1) - 1
-        # mid = 0
-        # while left < right:
-        #     mid = (left + right + 1) // 2
-        #     print(f"mid={mid}")
-
-        #     left_exists, right_exists = get_if_exist_child_nodes(root, mid)
-        #     print(f"{left_exists}, {right_exists}")
-
-        #     if (left_exists, right_exists) == (True, False):
-        #         print("stad return")
-        #         return nodes_sum + (2 * mid + 1)
-            
-        #     if (left_exists, right_exists) == (False, False):
-        #         right = mid - 1
-        #     else:
-        #         left = mid
-        
-        # return nodes_sum + left * 2
+            if does_node_exist(root, mid):
+                left = mid + 1
+            else:
+                right = mid
 
 
+        print(f"left={left}")
+        return nodes_sum + left
         
