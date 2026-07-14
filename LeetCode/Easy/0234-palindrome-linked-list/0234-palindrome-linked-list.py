1f1e5b87acde88273974aef96a9b2baf8f8a2022
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-14 20:25:44 +0200

    Time: 35 ms (54.59%), Space: 53.4 MB (36.56%) - LeetHub

--- LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,31 +5,37 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        def reverse_list(current: Optional[ListNode]) -> ListNode:
+        def find_after_mid_node(current: ListNode) -> ListNode:
+            slow = head
+            fast = head
+            while fast and fast.next:
+                fast = fast.next.next
+                slow = slow.next
+            
+            return slow
+
+        def reverse_list(current: ListNode) -> ListNode:
             previous = None
             while current:
                 current.next, previous, current = previous, current, current.next 
             
             return previous
 
-        slow = head
-        fast = head
-
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-
-        second_half_head = reverse_list(slow)
-        second_half_node = second_half_head
-        first_half_node = head
-        while second_half_node:
-            if first_half_node.val != second_half_node.val:
-                reverse_list(second_half_head)
-                return False
-            second_half_node = second_half_node.next
-            first_half_node = first_half_node.next
+        def compare_reversed_with_straight(reversed_half: ListNode, straight_half: ListNode) -> bool:
+            second_half_head = reversed_half
+            while reversed_half:
+                if straight_half.val != reversed_half.val:
+                    reverse_list(second_half_head)
+                    return False
+                reversed_half = reversed_half.next
+                straight_half = straight_half.next
         
-        reverse_list(second_half_head)
-        return True
+            reverse_list(second_half_head)
+            return True
+
+        second_half_head = reverse_list(find_after_mid_node(head))
+        return compare_reversed_with_straight(second_half_head, head)
+        
+        
 
         
