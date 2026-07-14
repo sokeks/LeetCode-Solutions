cf1c9ff8157d6cf97c45ead265c213c9e0d227a5
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-14 08:33:40 +0200

    Time: 23 ms (5.15%), Space: 24.9 MB (6.34%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        num_to_freqs: dict[int, int] = defaultdict(int)
+        freq_to_nums: list[list[int]] = [[] for _ in range(len(nums) + 1)]
+        idx = 0
+        while idx < len(nums):
+            anchor = idx
+            current_num = nums[anchor]
+            while idx < len(nums) and nums[idx] == current_num:
+                idx += 1
+            
+            num_to_freqs[current_num] += (idx - anchor)
+            freq_to_nums[num_to_freqs[current_num]].append(current_num)
+        
+        idx = len(freq_to_nums) - 1
+        top_k_freqs = set()
+        while idx >= 0 and len(top_k_freqs) < k:
+            top_k_freqs.update(freq_to_nums[idx])
+            idx -= 1
+
+        return list(top_k_freqs)
+
+
+        # O N logK (due to last loop) approach, sufficient for this task
         frequencies: dict[int, int] = defaultdict(int)
         
         for n in nums:
