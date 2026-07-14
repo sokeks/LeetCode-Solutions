0720d69593b20242d8751830a7718b8af431550c
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-14 08:01:36 +0200

    Time: 3 ms (89.55%), Space: 22.8 MB (80.44%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,3 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequencies: dict[int, int] = defaultdict(int)
+        
+        for n in nums:
+            frequencies[n] += 1
+        
+        top_k_freqs: list[tuple[int, int]] = []
+
+        for num, freq in frequencies.items():
+            if len(top_k_freqs) < k:
+                heapq.heappush(top_k_freqs, (freq, num))
+            elif top_k_freqs[0][0] < freq:
+                heapq.heapreplace(top_k_freqs, (freq, num))
+        
+        return [num for freq, num in top_k_freqs]
+
+
+        # pythonic, optimized by the Counter that calculates freq of each element in the collection
         return [num for num, freq in Counter(nums).most_common(k)]
