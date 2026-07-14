24cc3d42fedf0b9f20fde9e39068a0a8f4900660
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-14 21:08:34 +0200

    Time: 7 ms (60.15%), Space: 19.3 MB (27.82%) - LeetHub

---
+++ LeetCode/Easy/3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.py
@@ -0,0 +1,38 @@
+class Solution:
+    # with the original constraints, it's ok to do it brute force. I do this task for constraints much bigger
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        if k == 0: return 1
+        left = 0
+        right = 0
+        bit_set_count = [0 for _ in range(32)]
+        running_or = 0
+        min_length = float("+inf")
+
+        while right < len(nums):
+            while right < len(nums) and running_or < k:
+                num = nums[right]
+                right += 1
+                running_or |= num
+                
+                pos = 0
+                while num:
+                    num, rem = divmod(num, 2)
+                    bit_set_count[pos] += 1 if rem else 0
+                    pos += 1
+            
+
+            while left < right and running_or >= k:
+                min_length = min(min_length, right - left)
+                num = nums[left]
+                left += 1
+
+                pos = 0
+                while num:
+                    num, rem = divmod(num, 2)
+                    bit_set_count[pos] -= 1 if rem else 0
+                    if not bit_set_count[pos]:
+                        running_or &= ~(1 << pos)
+                    pos += 1
+        
+        return min_length if min_length != float("+inf") else -1
+                
