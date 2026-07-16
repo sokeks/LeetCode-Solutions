4075f29381b13f4f00aaeacdba538cd4c930f603
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-16 08:50:48 +0200

    Time: 11 ms (77.05%), Space: 19.3 MB (75.36%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        # non pythonic version for lowercase English letters
+        ALPHABET_SIZE = 26
+        chars = [0] * ALPHABET_SIZE
+        for c in s:
+            chars[ord(c) - ord('a')] += 1
+        
+        for c in t:
+            chars[ord(c) - ord('a')] -= 1
+
+        for c in chars:
+            if c != 0:
+                return False
+        
+        return True
+
+
+        # non pythonic version ready for unicode
         freqs_s = defaultdict(int)
         for c in s:
             freqs_s[c] += 1
