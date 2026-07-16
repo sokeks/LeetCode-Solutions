86744a100c308f5b281500409c6ca66358939182
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-16 08:42:12 +0200

    Time: 10 ms (79.25%), Space: 19.5 MB (29.35%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        freqs_s = defaultdict(int)
+        for c in s:
+            freqs_s[c] += 1
+        
+        freqs_t = defaultdict(int)
+        for c in t:
+            freqs_t[c] += 1
+
+        return freqs_s == freqs_t
+
+        # quick pythonic one
         return Counter(s) == Counter(t)
         
