dc5741152a97629d750eab93f58f3b6ade1e6d3a
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-16 08:54:12 +0200

    Time: 11 ms (77.05%), Space: 19.4 MB (75.36%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -3,31 +3,29 @@ class Solution:
         if len(s) != len(t):
             return False
         # non pythonic version for lowercase English letters
-        ALPHABET_SIZE = 26
-        chars = [0] * ALPHABET_SIZE
-        for c in s:
-            chars[ord(c) - ord('a')] += 1
+        # ALPHABET_SIZE = 26
+        # chars = [0] * ALPHABET_SIZE
+        # for c in s:
+        #     chars[ord(c) - ord('a')] += 1
         
-        for c in t:
-            chars[ord(c) - ord('a')] -= 1
+        # for c in t:
+        #     chars[ord(c) - ord('a')] -= 1
 
-        for c in chars:
-            if c != 0:
-                return False
+        # for c in chars:
+        #     if c != 0:
+        #         return False
         
-        return True
-
+        # return True
 
         # non pythonic version ready for unicode
-        freqs_s = defaultdict(int)
+        freqs = defaultdict(int)
         for c in s:
-            freqs_s[c] += 1
+            freqs[c] += 1
         
-        freqs_t = defaultdict(int)
         for c in t:
-            freqs_t[c] += 1
+            freqs[c] -= 1
 
-        return freqs_s == freqs_t
+        return all(f == 0 for f in freqs.values())
 
         # quick pythonic one
         return Counter(s) == Counter(t)
