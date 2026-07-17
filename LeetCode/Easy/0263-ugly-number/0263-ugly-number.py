9fd1209755cc9a33d450f5fb4c4622d5259e6121
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-17 08:47:25 +0200

    Time: 3 ms (15.96%), Space: 19.3 MB (29.12%) - LeetHub

--- LeetCode/Easy/0263-ugly-number/0263-ugly-number.py
+++ LeetCode/Easy/0263-ugly-number/0263-ugly-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
         if n <= 0: return False
+        
         prime_factors = (2, 3, 5)
-
         for prime in prime_factors:
             while n % prime == 0:
                 n //= prime
