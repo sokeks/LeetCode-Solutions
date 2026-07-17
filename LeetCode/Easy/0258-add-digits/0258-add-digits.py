4839770c993a40392fc1256337a944c38f0a29f2
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-17 08:22:30 +0200

    Time: 1 ms (22.64%), Space: 19.3 MB (17.29%) - LeetHub

--- LeetCode/Easy/0258-add-digits/0258-add-digits.py
+++ LeetCode/Easy/0258-add-digits/0258-add-digits.py
@@ -1,9 +1,13 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        # version from followup - golden
+        return (num - 1) % 9 + 1 if num else 0
+
+        # version from followup - my thinking
         result = num % 9
         return result if result != 0 or num == 0 else 9 
 
-
+        # standard version
         while num // 10:
             result = 0
             while num:
