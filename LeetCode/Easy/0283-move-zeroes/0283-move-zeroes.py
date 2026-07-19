f740c8a5f0d5138e0175cc9e609df426a5117ef0
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-19 19:45:19 +0200

    Time: 0 ms (100%), Space: 20.4 MB (89.37%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
@@ -4,13 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         write_idx = 0
-        zeros_count = 0
         for n in nums:
             if n != 0:
                 nums[write_idx] = n
                 write_idx += 1
-            else:
-                zeros_count += 1
-        
-        for i in range(1, zeros_count + 1):
-            nums[-i] = 0
+
+        for i in range(write_idx, len(nums)):
+            nums[i] = 0
