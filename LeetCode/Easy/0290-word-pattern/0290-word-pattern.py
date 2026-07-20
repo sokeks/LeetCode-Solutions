16dc574d7196e7347929353fa36a2241fb382525
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-20 11:18:39 +0200

    Time: 0 ms (100%), Space: 19.3 MB (68.74%) - LeetHub

--- LeetCode/Easy/0290-word-pattern/0290-word-pattern.py
+++ LeetCode/Easy/0290-word-pattern/0290-word-pattern.py
@@ -1,24 +1,21 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        s_splitted = s.split(" ")
-        if len(pattern) != len(s_splitted):
+        s_words = s.split(" ")
+        if len(pattern) != len(s_words):
             return False
 
-        ALPHABET_SIZE = 26
-        char_to_words = [""] * ALPHABET_SIZE
-        word_to_chars = {}
+        char_to_words = {}
+        seen_words = set()
 
-        for c, word in zip(pattern, s_splitted):
-            if char_to_words[ord(c) - ord('a')] != "":
-                if char_to_words[ord(c) - ord('a')] != word:
+        for c, word in zip(pattern, s_words):
+            existing_word = char_to_words.get(c)
+            if existing_word:
+                if existing_word != word:
                     return False
             else:
-                char_to_words[ord(c) - ord('a')] = word
-            
-            if word in word_to_chars:
-                if word_to_chars[word] != c:
+                if word in seen_words:
                     return False
-            else:
-                word_to_chars[word] = c
+                char_to_words[c] = word
+                seen_words.add(word)
         
         return True
