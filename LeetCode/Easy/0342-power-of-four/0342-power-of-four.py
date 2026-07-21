ff4659ac70d284e092d019eda73db5257d7473e7
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-21 10:35:32 +0200

    Time: 0 ms (100%), Space: 19.1 MB (98.01%) - LeetHub

--- LeetCode/Easy/0342-power-of-four/0342-power-of-four.py
+++ LeetCode/Easy/0342-power-of-four/0342-power-of-four.py
@@ -3,4 +3,8 @@ class Solution:
         if n <= 0:
             return False
         
+        # with modulo 3
+        return n & (n - 1) == 0 and n % 3 == 1
+        
+        # with bit mask
         return n & (n - 1) == 0 and n & 0x55555555 != 0
