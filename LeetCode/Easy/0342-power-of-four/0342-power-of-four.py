26ccc344ebf1c705cb38ca9192b7901871757102
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-21 10:27:52 +0200

    Time: 0 ms (100%), Space: 19.2 MB (88.2%) - LeetHub

---
+++ LeetCode/Easy/0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:    
+        if n <= 0:
+            return False
+        
+        return n & (n - 1) == 0 and n & 0x55555555 != 0
+        
