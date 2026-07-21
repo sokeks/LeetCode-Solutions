6b78b4b5320101cc0ac2a094cbe3f196f81a528c
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-21 10:22:43 +0200

    Time: 7 ms (68.67%), Space: 19.1 MB (97.97%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
@@ -2,9 +2,13 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n <= 0:
             return False
+        
+        # optimized O(1) solution
+        max_32_bits_power_of_3 = 1162261467
+        return 1162261467 % n == 0
 
+        # standard algorythmic O (log_3 n)
         while n % 3 == 0:
             n //= 3
         
         return n == 1
-        
