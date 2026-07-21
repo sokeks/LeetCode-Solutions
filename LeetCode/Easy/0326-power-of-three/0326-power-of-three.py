e969b43b4476a060398862f10627d1fa9a128aef
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-21 10:22:50 +0200

    Time: 11 ms (43.25%), Space: 19.1 MB (88.83%) - LeetHub

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.py
@@ -5,7 +5,7 @@ class Solution:
         
         # optimized O(1) solution
         max_32_bits_power_of_3 = 1162261467
-        return 1162261467 % n == 0
+        return max_32_bits_power_of_3 % n == 0
 
         # standard algorythmic O (log_3 n)
         while n % 3 == 0:
