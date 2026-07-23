17d61bc5db0b485b62e9731ad343033255a8ea1e
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-23 14:04:29 +0200

    Time: 1018 ms (17.24%), Space: 318.6 MB (5.81%) - LeetHub

--- LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,10 +1,34 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        lcs = [0] * (len(text2) + 1)
+    # iterative approach with compressed memory approach
+    #
+    # lcs table (compressed then to 1 row)
+    # |   | 0 | t | e | x | t | 2 |
+    # | t |
+    # | e |
+    # | x |
+    # | t |
+    # | 1 |
+    # 
+    # def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    #     lcs = [0] * (len(text2) + 1)
 
-        for r, t1 in enumerate(text1):
-            prev_diagonal = lcs[0]
-            for c, t2 in enumerate(text2):
-                prev_diagonal, lcs[c + 1] = lcs[c + 1], (prev_diagonal + 1) if t1 == t2 else max(lcs[c], lcs[c + 1])
+    #     for r, t1 in enumerate(text1):
+    #         prev_diagonal = lcs[0]
+    #         for c, t2 in enumerate(text2):
+    #             prev_diagonal, lcs[c + 1] = lcs[c + 1], (prev_diagonal + 1) if t1 == t2 else max(lcs[c], lcs[c + 1])
             
-        return lcs[-1]
+    #     return lcs[-1]
+    
+    # recursive approach
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        @cache
+        def lcsRec(r: int, c: int) -> int:
+            if r < 0 or c < 0:
+                return 0
+            
+            if text1[r] == text2[c]:
+                return lcsRec(r - 1, c - 1) + 1
+            else:
+                return max(lcsRec(r, c - 1), lcsRec(r - 1, c))
+        
+        return lcsRec(len(text1) - 1, len(text2) - 1)
