0f395d2792973c2f587f6c43b381dae19852b76f
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-24 09:45:16 +0200

    Time: 55 ms (76.36%), Space: 26.2 MB (76.72%) - LeetHub

--- LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ LeetCode/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -4,7 +4,6 @@ class Solution:
         with_stock = -prices[0]
 
         for stock_price in islice(prices, 1, None):
-            without_stock = max(without_stock, with_stock + stock_price - fee)
-            with_stock =  max(with_stock, without_stock - stock_price)
+            without_stock, with_stock = max(without_stock, with_stock + stock_price - fee), max(with_stock, without_stock - stock_price)
         
         return without_stock
