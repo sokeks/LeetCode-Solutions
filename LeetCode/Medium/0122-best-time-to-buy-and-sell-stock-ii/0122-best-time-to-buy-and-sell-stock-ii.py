e4a06f2e637e018b8fc76384c80f7b051d9c9332
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-24 09:56:01 +0200

    Time: 4 ms (28.51%), Space: 20.5 MB (10.53%) - LeetHub

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        
+        
+        # solution coming from DP - 714. Best Time to Buy and Sell Stock with Transaction Fee
         without_stock = 0
         with_stock = -prices[0]
 
@@ -7,4 +10,3 @@ class Solution:
             without_stock, with_stock = max(without_stock, with_stock + price), max(without_stock - price, with_stock)
 
         return without_stock
-        
