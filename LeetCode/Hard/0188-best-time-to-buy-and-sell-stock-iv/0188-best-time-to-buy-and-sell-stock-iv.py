26e726a260064fb27d15ac1be639ed2f58e272c9
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-28 10:03:23 +0200

    Time: 31 ms (65.42%), Space: 19.2 MB (98.34%) - LeetHub

--- LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,11 +1,23 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        bought_stocks = [-prices[0]] * k
-        sold_stocks = [0] * (k + 1)
+        def max_profit_of_limited_options():
+            bought_stocks = [-prices[0]] * k
+            sold_stocks = [0] * (k + 1)
 
-        for p in islice(prices, 1, None):
-            for i in range(1, k + 1):
-                sold_stocks[-i] = max(bought_stocks[-i] + p, sold_stocks[-i])
-                bought_stocks[-i] = max(sold_stocks[-i - 1] - p, bought_stocks[-i])
+            for p in islice(prices, 1, None):
+                for i in range(k - 1, -1, -1):
+                    sold_stocks[i + 1] = max(bought_stocks[i] + p, sold_stocks[i + 1])
+                    bought_stocks[i] = max(sold_stocks[i] - p, bought_stocks[i])
 
-        return max(sold_stocks)
+            return max(sold_stocks)
+
+        def max_profit_of_unlimited_options():
+            without_stock = 0
+            with_stock = -prices[0]
+
+            for p in islice(prices, 1, None):
+                without_stock, with_stock = max(without_stock, with_stock + p), max(with_stock, without_stock - p)
+            
+            return without_stock
+
+        return max_profit_of_limited_options() if k < len(prices) // 2 else max_profit_of_unlimited_options()
