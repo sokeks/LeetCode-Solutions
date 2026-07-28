4b6aeac2dbc5b8f39fb41239db750d8551de671a
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-28 10:52:55 +0200

    Time: 103 ms (97.3%), Space: 33.7 MB (10.81%) - LeetHub

--- LeetCode/Medium/2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.py
+++ LeetCode/Medium/2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.py
@@ -1,24 +1,20 @@
 class Solution:
     def sumOfBeauties(self, nums: List[int]) -> int:
-        mins: list[tuple[int, int]] = [(nums[-1], 1)]
+        mins: list[int] = [0] * len(nums)
+        mins[-1] = nums[-1]
         for i in range(len(nums) - 2, 1, -1):
-            if nums[i] >= mins[-1][0]:
-                mins[-1] = (mins[-1][0], mins[-1][1] + 1)
+            if nums[i] >= mins[i + 1]:
+                mins[i] = mins[i + 1]
             else:
-                mins.append((nums[i], 1))
+                mins[i] = nums[i]
 
         beauty_sum = 0
         max_seen = nums[0]
         for i in range(1, len(nums) - 1):
-            if nums[i] > max_seen and nums[i] < mins[-1][0]:
+            if max_seen < nums[i] < mins[i + 1]:
                 beauty_sum += 2
-            elif nums[i] > nums[i - 1] and nums[i] < nums[i + 1]:
+            elif nums[i - 1] < nums[i] < nums[i + 1]:
                 beauty_sum += 1
-            
-            if mins[-1][1] > 1:
-                mins[-1] = (mins[-1][0], mins[-1][1] - 1)
-            else:
-                mins.pop()
 
             if nums[i] > max_seen:
                 max_seen = nums[i]
