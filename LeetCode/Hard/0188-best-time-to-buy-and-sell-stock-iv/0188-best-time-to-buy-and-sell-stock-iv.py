deaa179d16ce1b4005cab41fb36b203e7cfd8a8e
sokeks <131011732+sokeks@users.noreply.github.com>
2026-07-28 07:47:14 +0200

    Time: 50 ms (46.44%), Space: 19.4 MB (56.18%) - LeetHub

--- LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -4,9 +4,8 @@ class Solution:
         sold_stocks = [0] * (k + 1)
 
         for p in islice(prices, 1, None):
-            for i in range(k):
-                sold_stocks[i] = max(bought_stocks[i] + p, sold_stocks[i])
-                bought_stocks[i] = max(sold_stocks[i + 1] - p, bought_stocks[i])
+            for i in range(1, k + 1):
+                sold_stocks[-i] = max(bought_stocks[-i] + p, sold_stocks[-i])
+                bought_stocks[-i] = max(sold_stocks[-i - 1] - p, bought_stocks[-i])
 
         return max(sold_stocks)
-        
