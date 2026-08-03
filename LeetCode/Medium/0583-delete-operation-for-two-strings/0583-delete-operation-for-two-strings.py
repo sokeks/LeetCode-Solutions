e8a02f6c8b681a974de4129734ef76aa33463fed
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-03 17:19:55 +0200

    Time: 72 ms (82%), Space: 19.4 MB (87.88%) - LeetHub

--- LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -5,6 +5,19 @@ class Solution:
     # 1. if word1[-1] == word[-1], we cut off both last chars and compare the last but ones
     # 2. if word1[-1] != word[-1], we know we need to delete somethng (so +1) and we ask what's the minimum of 2 paths - if we cut off word1's last char, or word2's.
     def minDistance(self, word1: str, word2: str) -> int:
+        source, target = (word1, word2) if len(word1) > len(word2) else (word2, word1)
+        source_len, target_len = len(source), len(target)
+        
+        dp = list(range(target_len + 1))
+        for i in range(1, source_len + 1):
+            prev_diagonal, dp[0] = dp[0], i
+            for j in range(1, target_len + 1):
+                prev_diagonal, dp[j] = dp[j], prev_diagonal if source[i - 1] == target[j - 1] else 1 + min(dp[j], dp[j - 1])
+            
+        return dp[-1]
+
+
+
         @cache
         def min_distance_rec(i: int, j: int) -> int:
             if i == 0:
