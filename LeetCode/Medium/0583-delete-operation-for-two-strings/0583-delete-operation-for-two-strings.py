4d278f5ea08bdac7d079f2a3164c1f081545cce5
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-03 17:08:14 +0200

    Time: 143 ms (13.59%), Space: 61.4 MB (5.19%) - LeetHub

--- LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ LeetCode/Medium/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,4 +1,9 @@
 class Solution:
+    # typical 2D DP task with rules:
+    # 0. We want to know how the least deletes to do for whole 2 strings, and number of deletes for last chars of both strings equals number of deletes for one but last
+    #    + if we need to delete the last ones.
+    # 1. if word1[-1] == word[-1], we cut off both last chars and compare the last but ones
+    # 2. if word1[-1] != word[-1], we know we need to delete somethng (so +1) and we ask what's the minimum of 2 paths - if we cut off word1's last char, or word2's.
     def minDistance(self, word1: str, word2: str) -> int:
         @cache
         def min_distance_rec(i: int, j: int) -> int:
