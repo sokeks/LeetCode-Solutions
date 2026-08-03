50c54a701d38cb4e0cf0a24eba7a3485503b6cb2
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-03 16:23:01 +0200

    Time: 29 ms (91.18%), Space: 19.2 MB (97.31%) - LeetHub

--- LeetCode/Medium/0072-edit-distance/0072-edit-distance.py
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.py
@@ -16,15 +16,15 @@ class Solution:
 
         # return min_distance_rec(len(word1), len(word2))
 
-        len_1 = len(word1)
-        len_2 = len(word2)
+        source, target = (word1, word2) if len(word1) > len(word2) else (word2, word1)
+        source_len, target_len = len(source), len(target)
         # memory optimal, see below for extended and easier to understand dp version
-        dp = list(range(len_2 + 1))
+        dp = list(range(target_len + 1))
         
-        for i in range(1, len_1 + 1):
+        for i in range(1, source_len + 1):
             prev_diagonal, dp[0] = dp[0], i
-            for j in range(1, len_2 + 1):
-                if word1[i - 1] == word2[j - 1]:
+            for j in range(1, target_len + 1):
+                if source[i - 1] == target[j - 1]:
                     dp[j], prev_diagonal = prev_diagonal, dp[j]
                 else:
                     dp[j], prev_diagonal = 1 + min(prev_diagonal, dp[j], dp[j - 1]), dp[j]
@@ -33,16 +33,16 @@ class Solution:
         
         # memory non-optimal
         # dp[i][j] - what's the fewest way to convert first i chars of word1 into first j chars of word2, including empty string (represented by + 1 factor)
-        dp = [[0] * (len_2 + 1) for _ in range(len_1 + 1)]
+        dp = [[0] * (target_len + 1) for _ in range(source_len + 1)]
 
-        for i in range(len_2 + 1):
+        for i in range(target_len + 1):
             dp[0][i] = i
         
-        for i in range(len_1 + 1):
+        for i in range(source_len + 1):
             dp[i][0] = i
         
-        for i in range(1, len_1 + 1):
-            for j in range(1, len_2 + 1):
+        for i in range(1, source_len + 1):
+            for j in range(1, target_len + 1):
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
