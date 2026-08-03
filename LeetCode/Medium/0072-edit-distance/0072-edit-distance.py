211abb1d2193c340d6c00fe3ae2419966e954729
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-03 10:48:28 +0200

    Time: 31 ms (89.7%), Space: 19.5 MB (89.87%) - LeetHub

--- LeetCode/Medium/0072-edit-distance/0072-edit-distance.py
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.py
@@ -1,9 +1,25 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        # memory non-optimal
         len_1 = len(word1)
         len_2 = len(word2)
-        # dp[i][j] - what's the fewest way to convert first i chars of word1 into first j chars of word2
+        # memory optimal, see below for extended and easier to understand dp version
+        dp = [0] * (len_2 + 1)
+
+        for i in range(len_2 + 1):
+            dp[i] = i
+        
+        for i in range(1, len_1 + 1):
+            prev_diagonal, dp[0] = dp[0], i
+            for j in range(1, len_2 + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[j], prev_diagonal = prev_diagonal, dp[j]
+                else:
+                    dp[j], prev_diagonal = 1 + min(prev_diagonal, dp[j], dp[j - 1]), dp[j]
+        
+        return dp[-1]
+        
+        # memory non-optimal
+        # dp[i][j] - what's the fewest way to convert first i chars of word1 into first j chars of word2, including empty string (represented by + 1 factor)
         dp = [[0] * (len_2 + 1) for _ in range(len_1 + 1)]
 
         for i in range(len_2 + 1):
