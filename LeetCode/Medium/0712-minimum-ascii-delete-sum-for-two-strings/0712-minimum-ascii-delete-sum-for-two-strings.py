36bdb2c716315c2e079ed539a0b212e85e4c4e38
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-04 10:32:36 +0200

    Time: 670 ms (19.64%), Space: 226.2 MB (5.51%) - LeetHub

--- LeetCode/Medium/0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ LeetCode/Medium/0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -2,6 +2,27 @@ class Solution:
     # this task is a modification of "583. Delete Operation for Two Strings" (see there for more explanations), when one has to just calculate min
     # number of operations, here it's minimum sum of ascii
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        asciis_1, asciis_2 = (ord(s1[i]) for i in range(len(s1))), (ord(s2[i]) for i in range(len(s2)))
+        asciis_sum_1, asciis_sum_2 = list(accumulate(asciis_1, initial=0)), list(accumulate(asciis_2, initial=0))
+
+        @cache
+        def minimumDeleteSum_rec(i_1: int, i_2: int):
+            if i_1 == 0:
+                return asciis_sum_2[i_2]
+            if i_2 == 0:
+                return asciis_sum_1[i_1]
+            
+            if s1[i_1 - 1] == s2[i_2 - 1]:
+                return minimumDeleteSum_rec(i_1 - 1, i_2 - 1)
+            else:
+                return min(ord(s1[i_1 - 1]) + minimumDeleteSum_rec(i_1 - 1, i_2), ord(s2[i_2 - 1]) + minimumDeleteSum_rec(i_1, i_2 - 1))
+        
+        return minimumDeleteSum_rec(len(s1), len(s2))
+            
+
+    # iterative version, memory optimal
+    # target -> columns
+    # source -> rows
         source, target = (s1, s2) if len(s1) >= len(s2) else (s2, s1)
         source_len, target_len = len(source), len(target)
 
