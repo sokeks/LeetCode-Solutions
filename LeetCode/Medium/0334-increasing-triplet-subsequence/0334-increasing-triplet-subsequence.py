46e140b3908fe6e8913809decabbd795e397caeb
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-05 20:29:15 +0200

    Time: 11 ms (95.22%), Space: 38.8 MB (67.03%) - LeetHub

--- LeetCode/Medium/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ LeetCode/Medium/0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -2,15 +2,15 @@ class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         if len(nums) < 3:
             return False
-        num_1 = float("inf")
-        num_2 = float("inf")
+        smallest = float("inf")
+        medium = float("inf")
 
         for n in nums:
-            if num_1 >= n:
-                num_1 = n
-            elif num_2 >= n:
-                num_2 = n
-            elif n > num_2:
+            if smallest >= n:
+                smallest = n
+            elif medium >= n:
+                medium = n
+            elif n > medium:
                 return True
 
         return False
