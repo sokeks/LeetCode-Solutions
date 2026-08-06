40ed5976e551746fee82084dbbeb6560a4266570
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-06 09:15:19 +0200

    Time: 39 ms (93.21%), Space: 19.1 MB (98.64%) - LeetHub

--- LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.py
+++ LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -4,14 +4,14 @@ class Solution:
         shorter_len, longer_len = len(shorter), len(longer)
 
         # how many connections made till getting to this level
-        dp = [[0] * (len(shorter) + 1) for _ in range(len(longer) + 1)]
+        dp = [0] * (len(shorter) + 1)
         
         for i in range(1, longer_len + 1):
-            # prev_diagonal = dp[i][0]
+            prev_diagonal = dp[0]
             for j in range(1, shorter_len + 1):
-                if longer[i - 1] == shorter[j - 1] and dp[i - 1][j] == dp[i][j - 1] == dp[i - 1][j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j]
+                if longer[i - 1] == shorter[j - 1] and dp[j] == dp[j - 1] == prev_diagonal:
+                    prev_diagonal, dp[j] = dp[j], 1 + dp[j]
                 else:
-                    dp[i][j] = dp[i - 1][j] if dp[i - 1][j] > dp[i][j - 1] else dp[i][j - 1]
+                    prev_diagonal, dp[j] = dp[j], dp[j] if dp[j] > dp[j - 1] else dp[j - 1]
 
-        return dp[-1][-1]
+        return dp[-1]
