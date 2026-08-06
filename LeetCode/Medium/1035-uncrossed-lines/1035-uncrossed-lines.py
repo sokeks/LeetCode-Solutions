3ef03f5ad3fa0ecfc4fd6fdf9305aa9c1132a9ad
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-06 09:21:26 +0200

    Time: 39 ms (93.21%), Space: 19.2 MB (98.64%) - LeetHub

--- LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.py
+++ LeetCode/Medium/1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        shorter, longer = (nums1, nums2) if nums1 <= nums2 else (nums2, nums1)
+        shorter, longer = (nums1, nums2) if len(nums1) <= len(nums2) else (nums2, nums1)
         shorter_len, longer_len = len(shorter), len(longer)
 
         # how many connections made till getting to this level
@@ -9,7 +9,7 @@ class Solution:
         for i in range(1, longer_len + 1):
             prev_diagonal = dp[0]
             for j in range(1, shorter_len + 1):
-                if longer[i - 1] == shorter[j - 1] and dp[j] == dp[j - 1] == prev_diagonal:
+                if longer[i - 1] == shorter[j - 1] and dp[j] == prev_diagonal:
                     prev_diagonal, dp[j] = dp[j], 1 + dp[j]
                 else:
                     prev_diagonal, dp[j] = dp[j], dp[j] if dp[j] > dp[j - 1] else dp[j - 1]
