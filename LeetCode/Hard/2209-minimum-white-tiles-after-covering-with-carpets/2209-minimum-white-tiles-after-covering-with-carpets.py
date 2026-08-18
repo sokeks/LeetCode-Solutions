b1068feded425977aea106b8bd78dcd53bfd98c9
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-18 10:02:58 +0200

    Time: 702 ms (98.39%), Space: 19.5 MB (93.55%) - LeetHub

--- LeetCode/Hard/2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
+++ LeetCode/Hard/2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
@@ -1,13 +1,12 @@
 class Solution:
     def minimumWhiteTiles(self, floor: str, numCarpets: int, carpetLen: int) -> int:
-        def tile_value(tile: str) -> int:
-            return 1 if tile == '1' else 0
-        dp_previous = list(accumulate(floor, lambda white_tiles_sum, tile : white_tiles_sum + tile_value(tile), initial=0,))
+        floor_ints = [1 if tile == '1' else 0 for tile in floor]
+        dp_previous = list(accumulate(floor_ints, lambda white_tiles_sum, tile : white_tiles_sum + tile, initial=0,))
         dp_current = [0] * len(dp_previous)
         
         for c in range(1, numCarpets + 1):
-            for i in range(1, len(dp_previous)):
-                dp_current[i] = min(dp_current[i - 1] + tile_value(floor[i - 1]), dp_previous[max(0, i - carpetLen)])
+            for i in range(c * carpetLen, len(dp_previous)):
+                dp_current[i] = min(dp_current[i - 1] + floor_ints[i - 1], dp_previous[max(0, i - carpetLen)])
             dp_previous, dp_current = dp_current, [0] * len(dp_previous)
 
         return dp_previous[-1]        
