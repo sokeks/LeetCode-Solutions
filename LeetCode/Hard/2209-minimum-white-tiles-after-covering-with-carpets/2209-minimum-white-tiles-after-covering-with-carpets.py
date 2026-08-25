7222e63432b895b307baf7d61824bd314ee228b9
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-25 08:43:35 +0200

    Time: 3447 ms (50%), Space: 490.1 MB (48.57%) - LeetHub

--- LeetCode/Hard/2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
+++ LeetCode/Hard/2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
@@ -1,13 +1,14 @@
 class Solution:
     def minimumWhiteTiles(self, floor: str, numCarpets: int, carpetLen: int) -> int:
         floor_ints = [int(tile) for tile in floor]
-    # dp using recurence
+    # dp using recurence, less optimal, however cleaner and easier to understand
+        floor_prefixes = list(accumulate(floor_ints))
         @cache
         def minimum_white_tiles_rec(tile_idx: int, carpets: int) -> int:
             if tile_idx < 0:
                 return 0
             if carpets == 0:
-                return sum(itertools.islice(floor_ints, tile_idx + 1))
+                return floor_prefixes[tile_idx]
             
             return min(minimum_white_tiles_rec(tile_idx - carpetLen, carpets - 1), floor_ints[tile_idx] + minimum_white_tiles_rec(tile_idx - 1, carpets))
 
