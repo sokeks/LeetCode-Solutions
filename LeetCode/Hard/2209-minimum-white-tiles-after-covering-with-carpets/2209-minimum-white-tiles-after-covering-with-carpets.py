afa2801adf24b4b5bfa6bbe9c48be5eda97d6252
sokeks <131011732+sokeks@users.noreply.github.com>
2026-08-25 08:35:56 +0200

    Time: 4153 ms (15.71%), Space: 495.4 MB (48.57%) - LeetHub

--- LeetCode/Hard/2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
+++ LeetCode/Hard/2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
@@ -1,8 +1,19 @@
 class Solution:
     def minimumWhiteTiles(self, floor: str, numCarpets: int, carpetLen: int) -> int:
+        floor_ints = [int(tile) for tile in floor]
+    # dp using recurence
+        @cache
+        def minimum_white_tiles_rec(tile_idx: int, carpets: int) -> int:
+            if tile_idx < 0:
+                return 0
+            if carpets == 0:
+                return sum(itertools.islice(floor_ints, tile_idx + 1))
+            
+            return min(minimum_white_tiles_rec(tile_idx - carpetLen, carpets - 1), floor_ints[tile_idx] + minimum_white_tiles_rec(tile_idx - 1, carpets))
+
+        return minimum_white_tiles_rec(len(floor_ints) - 1, numCarpets)
     
     #dp optimized for memory
-        floor_ints = [int(tile) for tile in floor]
         # dp answers: how many white carpets (represented as '1') are visible for floor length under index (i-th) for that many carpets (calculate from 1 to numCarpets step by step)
         dp_previous = list(accumulate(floor_ints, initial=0))
         dp_current = [0] * len(dp_previous)
