55c6df2f80b306680124f4602aaa7317810082db
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-06 12:39:50 +0200

    Time: 46 ms (88.45%), Space: 20.4 MB (5.94%) - LeetHub

--- LeetCode/Hard/3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.py
+++ LeetCode/Hard/3474-lexicographically-smallest-generated-string/3474-lexicographically-smallest-generated-string.py
@@ -1,28 +1,29 @@
 class Solution:
     def generateString(self, str1: str, str2: str) -> str:
-        result = ['a'] * (len(str1) + len(str2) - 1)
-        set_idxs = set()
+        len_str2 = len(str2)
+        result = ['a'] * (len(str1) + len_str2 - 1)
+        committed = set()
 
         for i, c in enumerate(str1):
             if c == 'F':
                 continue
-            for j in range(len(str2)):
+            for j in range(len_str2):
                 w = i + j
-                if w in set_idxs and result[w] != str2[j]:
+                if w in committed and result[w] != str2[j]:
                     return ""
                 result[w] = str2[j]
-                set_idxs.add(w)
+                committed.add(w)
         
         str2_list = list(str2)
         for i, c in enumerate(str1):
-            if c == 'T' or result[i:i + len(str2)] != str2_list:
+            if c == 'T' or result[i:i + len_str2] != str2_list:
                 continue
             
-            for j in range(len(str2) - 1, -1, -1):
+            for j in range(len_str2 - 1, -1, -1):
                 rightmost_idx = i + j
-                if not rightmost_idx in set_idxs:
+                if not rightmost_idx in committed:
                     result[rightmost_idx] = 'b'
-                    set_idxs.add(rightmost_idx)
+                    committed.add(rightmost_idx)
                     break
             else:
                 return ""
