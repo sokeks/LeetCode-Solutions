4ee433228ee21bb969095c6a4be79cdb623ebc62
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-07 11:24:38 +0200

    Time: 291 ms (98.92%), Space: 25 MB (82.26%) - LeetHub

--- LeetCode/Medium/3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.py
+++ LeetCode/Medium/3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.py
@@ -1,18 +1,18 @@
 class Solution:
     def clearStars(self, s: str) -> str:
-        chars_positions:dict[str, deque[int]] = defaultdict(deque)
-        sorted_chars = []
+        chars_positions:dict[str, list[int]] = defaultdict(list)
+        available_chars = []
 
         result = list(s)
         for i, c in enumerate(result):
             if c == '*':
-                smallest = sorted_chars[0]
-                result[chars_positions[smallest].pop()] = '*'
-                if not chars_positions[smallest]:
-                    heapq.heappop(sorted_chars)
+                char = available_chars[0]
+                result[chars_positions[char].pop()] = '*'
+                if not chars_positions[char]:
+                    heapq.heappop(available_chars)
             else:
                 if not chars_positions[c]:
-                    heapq.heappush(sorted_chars, c)
+                    heapq.heappush(available_chars, c)
                 
                 chars_positions[c].append(i)
 
