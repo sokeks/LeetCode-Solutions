52b88708386158544859de97ec2a007b0829da9d
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-08 10:25:16 +0200

    Time: 75 ms (73.33%), Space: 43.6 MB (17.71%) - LeetHub

--- LeetCode/Medium/3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
+++ LeetCode/Medium/3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     string clearStars(string s) {
-        priority_queue<char, vector<char>, greater<char>> available_chars_heap;
-        unordered_map<char, stack<size_t>> char_positions;
+        priority_queue<char, vector<char>, greater<char>> smallest_available_chars_heap;
+        stack<size_t> char_positions[26];
 
         string result = s;
         for (auto [i, c] : views::enumerate(result))
         {
             if (c != '*')
             {
-                if (char_positions[c].size() == 0)
+                if (char_positions[c - 'a'].size() == 0)
                 {
-                    available_chars_heap.push(c);
+                    smallest_available_chars_heap.push(c);
                 }
-                char_positions[c].push(i);
+                char_positions[c - 'a'].push(i);
             }
             else
             {
-                auto available_char = available_chars_heap.top();
-                result[char_positions[available_char].top()] = '*';
-                char_positions[available_char].pop();
+                auto smallest_char = smallest_available_chars_heap.top();
+                result[char_positions[smallest_char - 'a'].top()] = '*';
+                char_positions[smallest_char - 'a'].pop();
 
-                if (char_positions[available_char].size() == 0) available_chars_heap.pop();
+                if (char_positions[smallest_char - 'a'].empty()) smallest_available_chars_heap.pop();
             }
         }
 
