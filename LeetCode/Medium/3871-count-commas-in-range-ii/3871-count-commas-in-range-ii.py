185c08ecf99821460e29a2402e43202d619e5009
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-17 09:22:24 +0200

    Time: 4 ms (3.68%), Space: 19.3 MB (16.21%) - LeetHub

--- LeetCode/Medium/3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ LeetCode/Medium/3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -1,22 +1,11 @@
 class Solution:
     def countCommas(self, n: int) -> int:
         commas_count = 0
-        lower = 1000
-        commas_in_number = 1
+        threshold = 1000
 
-        while lower <= n:
-            commas_count += (min(n, lower * 1000 - 1) - lower + 1) * commas_in_number
-            lower *= 1000
-            commas_in_number += 1
-
-
-
-        # commas_count = 0
-        # threshold = 1000
-
-        # while threshold <= n:
-        #     commas_count += n - threshold + 1
-        #     threshold *= 1000
+        while threshold <= n:
+            commas_count += n - threshold + 1
+            threshold *= 1000
         
         return commas_count
 
