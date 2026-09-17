b3e76097fc76d3cdc1b5fdcf51b6e3ab2e9e4f05
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-17 09:17:38 +0200

    Time: 0 ms (100%), Space: 19.3 MB (16.21%) - LeetHub

--- LeetCode/Medium/3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ LeetCode/Medium/3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -1,29 +1,23 @@
 class Solution:
     def countCommas(self, n: int) -> int:
         commas_count = 0
-        threshold = 1000
+        lower = 1000
+        commas_in_number = 1
 
-        while threshold <= n:
-            commas_count += n - threshold + 1
-            threshold *= 1000
+        while lower <= n:
+            commas_count += (min(n, lower * 1000 - 1) - lower + 1) * commas_in_number
+            lower *= 1000
+            commas_in_number += 1
+
+
+
+        # commas_count = 0
+        # threshold = 1000
+
+        # while threshold <= n:
+        #     commas_count += n - threshold + 1
+        #     threshold *= 1000
         
         return commas_count
 
-        def get_order_of_magnitude(n: int) -> n:
-            return int(math.log(n, 1000))
-
-        div, mod = divmod(n, 1000)
-        if div == 0:
-            return 0
-        
-        order_of_magnitude = get_order_of_magnitude(n)
-
-        commas_count = 0
-        for order in orders_of_magnitude:
-            commas_count += (n - 1000**(order + 1))
-
-        
-        base = 1000 ** order_of_magnitude
-
-        return (div - 1) * 1000 + order_of_magnitude * ((mod + 1) + (n - (n // base) * base - mod) // 1000)
         
