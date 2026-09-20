8c57fc513a760a8e367b1348290f8ae5579333af
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-20 12:58:31 +0200

    Time: 1132 ms (40%), Space: 19.3 MB (91.43%) - LeetHub

---
+++ LeetCode/Hard/3579-minimum-steps-to-convert-string-with-operations/3579-minimum-steps-to-convert-string-with-operations.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minOperations(self, word1: str, word2: str) -> int:
+        def count_operations(source: str, target: str) -> int:
+            pair_mismatches = [[0] * 26 for _ in range(26)]
+            for s, t in zip(source, target):
+                if s != t:
+                    pair_mismatches[ord(s) - ord('a')][ord(t) - ord('a')] += 1
+            
+            operations_count = 0
+            for s, t in zip(source, target):
+                if s == t:
+                    continue
+                
+                s_idx = ord(s) - ord('a')
+                t_idx = ord(t) - ord('a')
+                if pair_mismatches[s_idx][t_idx] == 0:
+                    continue
+
+                operations_count += pair_mismatches[s_idx][t_idx] + pair_mismatches[t_idx][s_idx] - min(pair_mismatches[s_idx][t_idx], pair_mismatches[t_idx][s_idx])
+                pair_mismatches[s_idx][t_idx] = 0
+                pair_mismatches[t_idx][s_idx] = 0
+            
+            return operations_count
+
+        dp = []
+        for current_len in range(len(word1) + 1):
+            min_operations = current_len
+            for start in range(0, current_len):
+                source = word1[start:current_len]
+                target = word2[start:current_len]
+
+                current_operations = dp[start] + min(count_operations(source, target), 1 + count_operations(source[::-1], target))
+                min_operations = min_operations if min_operations < current_operations else current_operations
+                
+            dp.append(min_operations)
+
+        return dp[-1]
