1a8b13d098a423e1501e0efa77409d49dd742a26
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-21 15:03:27 +0200

    Time: 127 ms (44.9%), Space: 11.6 MB (83.67%) - LeetHub

--- LeetCode/Hard/3579-minimum-steps-to-convert-string-with-operations/3579-minimum-steps-to-convert-string-with-operations.cpp
+++ LeetCode/Hard/3579-minimum-steps-to-convert-string-with-operations/3579-minimum-steps-to-convert-string-with-operations.cpp
@@ -3,27 +3,27 @@ public:
     int minOperations(string word1, string word2) {
         vector dp(word1.length() + 1, 0);
 
-        for (auto currentLength = 1; currentLength <= word1.length(); ++currentLength)
+        for (auto prefixLength = 1uz; prefixLength <= word1.length(); ++prefixLength)
         {
-            auto minOperations = currentLength;
-            for (auto start = 0; start < currentLength; ++start)
+            auto minOperationsCount = prefixLength;
+            for (auto start = 0uz; start < prefixLength; ++start)
             {
-                string_view source(word1.data() + start, currentLength - start);
-                string_view target(word2.data() + start, currentLength - start);
+                string_view source(word1.data() + start, prefixLength - start);
+                string_view target(word2.data() + start, prefixLength - start);
 
                 auto nonReversedOperations = countReplacesAndSwaps(source, target);
                 auto reversedOperations = 1 + countReplacesAndSwaps(source | views::reverse, target);
 
-                minOperations = min(dp[start] + min(nonReversedOperations, reversedOperations), minOperations);
+                minOperationsCount = min(dp[start] + min(nonReversedOperations, reversedOperations), minOperationsCount);
             }
-            dp[currentLength] = minOperations;
+            dp[prefixLength] = minOperationsCount;
         }
 
         return dp.back();
     }
 private:
     template <typename R1, typename R2>
-    int countReplacesAndSwaps(R1&& source, R2&& target)
+    size_t countReplacesAndSwaps(R1&& source, R2&& target)
     {
         array<array<int, 26>, 26> pair_mismatches = {};
 
@@ -35,8 +35,8 @@ private:
             }
         }
 
-        auto operationsCount = 0;
-        for (auto s = 0; s < pair_mismatches.size(); ++s)
+        auto operationsCount = 0uz;
+        for (auto s = 0uz; s < pair_mismatches.size(); ++s)
         {
             for (auto t = s + 1; t < pair_mismatches.size(); ++t)
             {
