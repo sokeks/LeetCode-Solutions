e4cd7fe173fd1d3f321de6d6fa7acf1f5a05a575
sokeks <131011732+sokeks@users.noreply.github.com>
2026-09-23 12:17:40 +0200

    Time: 8 ms (99.49%), Space: 19.5 MB (8.1%) - LeetHub

--- LeetCode/Easy/1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ LeetCode/Easy/1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -1,6 +1,22 @@
 class Solution:
     def countQuadruplets(self, nums: list[int]) -> int:
         distinct_quadruplets_count = 0
+    # version for O(n^2)
+        sums_count = defaultdict(int)
+        for c in range(2, len(nums)):
+            b = c - 1
+            for a in range(b):
+                sums_count[nums[a] + nums[b]] += 1
+            
+            for d in range(c + 1, len(nums)):
+                fitting_sum = nums[d] - nums[c]
+                distinct_quadruplets_count += sums_count[fitting_sum]
+
+        return distinct_quadruplets_count
+
+
+        distinct_quadruplets_count = 0
+    # version for O(n^4)
         for a in range(len(nums)):
             for b in range(a + 1, len(nums)):
                 for c in range(b + 1, len(nums)):
